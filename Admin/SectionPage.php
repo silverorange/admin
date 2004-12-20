d9f209f3217a5e41bdd8975890a7f2366beac2ac
Nathan Fredrickson <nathan@silverorange.com>
2004-12-20 22:06:38 +0000

    Restructuring (I'm done, hopefully)
    svn commit r121

---
+++ Admin/SectionPage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+require_once('Admin/AdminPage.php');
+
+/**
+ * Class for section pages.
+ */
+class SectionPage extends AdminPage {
+
+	public function init() {
+
+	}
+
+	public function display() {
+		echo 'hello';
+	}
+	
+	public function process() {
+
+	}
+}
