0314ec35377d6f760ba43c8f65876708c8495677
Nathan Fredrickson <nathan@silverorange.com>
2005-01-07 23:33:27 +0000

    New experimental stuff.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@133 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminApplication.php
+++ Admin/AdminApplication.php
@@ -10,24 +10,36 @@ require_once('AdminPage.php');
 class AdminApplication extends SwatApplication {
 
 	public $db = null;
+	public $name;
 
-	function __construct() {
-		$this->connect_db();
+	function __construct($name) {
+		$this->name = $name;
+		$this->initDatabase();
+		$this->initSession();
 
 	}
+
 
 	/**
 	 * Get the page object.
 	 * Uses the $_GET variables to decide which page subclass to instantiate.
 	 * @return AdminPage A subclass of AdminPage is returned.
 	 */
-	public function get_page() {
+	public function getPage() {
 
 		if (isset($_GET['source']))
 			$source = $_GET['source'];
 		else
-			$source = 'Front';
+			$source = 'front';
 
+		//$this->queryForPage($source);
+
+		/*
+		if (!$this->isLoggedIn()) {
+			$component = 'login';
+			$subcomponent = 'index';
+		} elseif (strpos($source, '/')) {
+		*/
 		if (strpos($source, '/')) {
 			list($component, $subcomponent) = explode('/', $source);
 		} else {
@@ -35,33 +47,83 @@ class AdminApplication extends SwatApplication {
 			$subcomponent = 'Index';
 		}
 
-
 		echo "$component/$subcomponent<br>";
+		//if ($component == 'login')
+		//	$class = 'Admin/Login/Index';
 
-		switch ($component) {
-
-			case 'AdminSections':
-				require_once("Admin/$component/$subcomponent.php");
-				$classname = $component.$subcomponent;
-				$page = eval(sprintf("return new %s();", $classname));
-				break;
-
-			default:
-				// TODO: instatiate a "Not Found" page here.
-				echo 'not found';
-				exit();
-
-		}
+		require_once("Admin/$component/$subcomponent.php");
+		$classname = $component.$subcomponent;
+		$page = eval(sprintf("return new %s();", $classname));
 
 		return $page;
 	}
 
-	private function connect_db() {
+	private function queryForPage($source) {
+		$sql = <<<SQL
+			SELECT adminarticles.*, adminsections.title AS section_title
+			FROM adminarticles
+			INNER JOIN adminsections ON section = sectionID
+			WHERE adminarticles.hidden = 0
+				AND shortname = '$source'
+				AND articleID IN (
+					SELECT article
+					FROM adminarticle_admingroup
+					INNER JOIN adminuser_admingroup
+						ON adminarticle_admingroup.groupnum = adminuser_admingroup.groupnum
+					WHERE adminuser_admingroup.usernum = {$_SESSION['userID']}
+				)
+SQL;
+
+		$result = $this->db->query($sql);
+                                                                                                                             
+		if ($result->size() == 0) {
+			$sql = <<<SQL
+				SELECT adminarticles.*, adminsections.title AS section_title
+				FROM adminarticles
+				INNER JOIN adminsections ON section = sectionID
+				WHERE shortname = '$source_exp[0]'
+					AND articleID IN (
+						SELECT article
+						FROM adminarticle_admingroup
+						INNER JOIN adminuser_admingroup 
+							ON adminarticle_admingroup.groupnum = adminuser_admingroup.groupnum
+						WHERE adminuser_admingroup.usernum = {$_SESSION['userID']}
+					)
+SQL;
+			$result = $this->db->query($sql);
+
+			if ($result->numrows() == 0)
+				$result = null;
+		}
+
+		return $result;
+	}
+
+	private function initDatabase() {
 		// TODO: change to array form of DSN and move parts to a secure include file.
 		$dsn = "pgsql://php:test@zest/silverorange2";
 		$this->db = MDB2::connect($dsn);
 
 		if (MDB2::isError($this->db))
 			throw new Exception('Unable to connect to database.');
+	}
+
+	private function initSession() {
+		session_cache_limiter('');
+		session_save_path('/so/phpsessions/'.$this->name);
+		session_name($this->name);
+		session_start();
+
+		if (!isset($_SESSION['userID'])) {
+			$_SESSION['userID'] = 0;
+			$_SESSION['name'] = '';
+		}
+	}
+
+	public function isLoggedIn() {
+			if (isset($_SESSION['userID']))
+				return ($_SESSION['userID'] != 0);
+
+			return false;
 	}
 }
