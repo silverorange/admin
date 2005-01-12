7c682dc42aa850bc572c35c9d85682a51434ba64
Nick Burka <nick@silverorange.com>
2005-01-12 21:22:54 +0000

    Added subcomponents to the menu.
    svn commit r162

--- Admin/AdminPage.php
+++ Admin/AdminPage.php
@@ -34,13 +34,26 @@ abstract class AdminPage extends SwatPage {
 
 	public function displayMenu($app) {
 		$sql = "SELECT admincomponents.shortname, admincomponents.title,
-					admincomponents.section, adminsections.title AS sectiontitle
+					admincomponents.section, adminsections.title AS sectiontitle,
+					admincomponents.componentid,
+					adminsubcomponents.title as subcomponent_title,
+					adminsubcomponents.shortname as subcomponent_shortname
 				FROM admincomponents 
+
+				LEFT OUTER JOIN adminsubcomponents on
+					adminsubcomponents.subcomponentid = admincomponents.componentid
+
 				INNER JOIN adminsections ON
 					admincomponents.section = adminsections.sectionid
+
 				WHERE adminsections.hidden='0' --$app->db->quote(0,'bit')
 				
 				AND admincomponents.hidden='0' --$app->db->quote(0,'bit')
+
+				AND (
+					adminsubcomponents.hidden='0' --$app->db->quote(0,'bit')
+					OR adminsubcomponents.hidden is  null
+				)
 		";
 		// TODO: make this work once sessions are working
 				/*
@@ -55,30 +68,64 @@ abstract class AdminPage extends SwatPage {
 		$sql.="
 				ORDER BY adminsections.displayorder, adminsections.title,
 				admincomponents.section, admincomponents.displayorder,
-				admincomponents.title";
+				admincomponents.title, adminsubcomponents.displayorder,
+				adminsubcomponents.title";
 		
-		$types = array('text', 'text', 'integer','text');
+		$types = array('text', 'text', 'integer', 'text', 'integer', 'text', 'text');
 		$result = $app->db->query($sql, $types);
 		
 		if (MDB2::isError($result)) 
 			throw new Exception($result->getMessage());
 
 		$section_out = 0;
+		$component_out = 0;
 		$currentrow = 0;		
-		
+		$subcomponents = false;		
+
 		echo '<ul>';
 		while ($row = $result->fetchRow(MDB2_FETCHMODE_OBJECT)) {
 			$currentrow++;
 			if ($row->section != $section_out) {
+				if ($subcomponents) {
+					echo '</ul></li>';
+					$subcomponents = false;
+				}
+				
 				if ($currentrow != 1) echo "</li></ul>";
 				echo '<li><span>'.$row->sectiontitle.'</span>';
 				echo '<ul>';
+				$section_out = $row->section;
 			}
-			$section_out = $row->section;
-			echo '<li><a href="a/'.$row->shortname.'">';
-			echo $row->title;
-			echo '</a></li>';
+			
+			if ($row->componentid != $component_out) {
+				if ($subcomponents) {
+					echo '</ul></li>';
+					$subcomponents = false;
+				}
+
+				echo '<li><a href="a/'.$row->shortname.'">';
+				echo $row->title;
+				echo '</a>';
+
+				$component_out = $row->componentid;
+
+				if ($row->subcomponent_title != null) {
+					echo '<ul>';
+					$subcomponents = true;
+				} else {
+					echo '</li>';
+					$subcomponents = false;
+				}
+			}
+		
+			if ($row->subcomponent_title != null) {	
+				echo '<li><a href="a/'.$row->subcomponent_shortname.'">';
+				echo $row->subcomponent_title;
+				echo '</a></li>';
+			}
 		}
+		
+		if ($subcomponents) echo '</ul></li>';
 		echo '</ul></li></ul>';
 	}
 	
