5934058974d416cbe8e5251733afef8946253bfc
Nathan Fredrickson <nathan@silverorange.com>
2005-01-16 02:41:45 +0000

    Refactoring of the admin menu into it's own class.  Not totally happy yet, but a step in the right direction.  First time using an MDB2 custom wrapper class too.
    svn commit r164

---
+++ Admin/AdminMenu.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+
+/**
+ * The primary navigation in an admin.
+ */
+class AdminMenu {
+
+	private $sections;
+
+	function __construct($result) {
+
+		if (MDB2::isError($result)) 
+			throw new Exception($result->getMessage());
+
+		$this->sections = array();
+		$section = null;
+		$component = null;
+
+		while ($row = $result->fetchRow(MDB2_FETCHMODE_OBJECT)) {
+			if ($section == null || $row->section != $section->id) {
+				$section = new AdminMenuSection($row->section, $row->sectiontitle);
+				$this->sections[] = $section;
+			}
+
+			if ($component == null || $row->componentid != $component->id) {
+				$component = new AdminMenuComponent($row->componentid, 
+					$row->shortname, $row->title);
+				$section->components[] = $component;
+			} else {
+				$subcomponent = new AdminMenuSubcomponent($row->subcomponent_shortname, 
+					$row->subcomponent_title);
+				$component->subcomponents[] = $subcomponent;
+			}
+		}
+	}
+
+	public function display() {
+		echo '<ul>';
+
+		foreach ($this->sections as $section)
+			$section->display();
+
+		echo '</ul>';
+	}
+
+}
+
+/**
+ * Data class for a menu section.
+ */
+class AdminMenuSection {
+
+	public $id;
+	public $title;
+	public $components;
+
+	function __construct($id, $title) {
+		$this->id = $id;
+		$this->title = $title;
+		$this->components = array();
+	}
+
+	public function display() {
+		echo '<li><span>'.$this->title.'</span>';
+		echo '<ul>';
+
+		foreach ($this->components as $component)
+			$component->display();
+
+		echo '</ul>';
+	}
+}
+
+/**
+ * Data class for a menu component.
+ */
+class AdminMenuComponent {
+
+	public $id;
+	public $shortname;
+	public $title;
+	public $subcomponents;
+
+	function __construct($id, $shortname, $title) {
+		$this->id = $id;
+		$this->shortname = $shortname;
+		$this->title = $title;
+		$this->subcomponents = array();
+	}
+
+	public function display() {
+		echo '<li><a href="a/'.$this->shortname.'">';
+		echo $this->title;
+		echo '</a>';
+
+		if (count($this->subcomponents)) {
+			echo '<ul>';
+
+			foreach ($this->subcomponents as $subcomponent)
+				$subcomponent->display();
+
+			echo '</ul>';
+		}
+
+		echo '</li>';
+	}
+
+}
+
+/**
+ * Data class for a menu subcomponent.
+ */
+class AdminMenuSubcomponent {
+
+	public $shortname;
+	public $title;
+
+	function __construct($shortname, $title) {
+		$this->shortname = $shortname;
+		$this->title = $title;
+	}
+
+	public function display() {
+		echo '<li><a href="a/'.$this->shortname.'">';
+		echo $this->title;
+		echo '</a></li>';
+	}
+}
+
