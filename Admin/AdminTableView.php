22be753117e8d023a72800acd0b49ebbd9c84296
Nathan Fredrickson <nathan@silverorange.com>
2005-01-29 22:22:09 +0000

    Move SwatTableViewRow to AdminTableViewRow
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@245 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminTableView.php
+++ Admin/AdminTableView.php
@@ -4,14 +4,28 @@
  * @copyright silverorange 2004
  */
 require_once('Swat/SwatTableView.php');
+require_once('Admin/AdminTableViewRow.php');
 
 /**
  * Subclass of SwatTableView for admin indexes.
  */
 class AdminTableView extends SwatTableView {
 
-	function __construct() {
-		parent::__construct();
+    private $extra_rows;
 
+	public function __init() {
+		parent::init();
+		$this->extra_rows = array();
+	}
+
+	public function appendRow(AdminTableViewRow $row) {
+		$this->extra_rows[] = $row;
+	}
+
+	protected function displayContent() {
+		parent::displayContent();
+
+		foreach ($this->extra_rows as $row)
+			$row->display($this->columns);
 	}
 }
