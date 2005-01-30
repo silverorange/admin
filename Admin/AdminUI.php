045e79cc7f0d6b87e216f5db1943c02574a27a83
Nathan Fredrickson <nathan@silverorange.com>
2005-01-30 16:20:02 +0000

    reflect changes in Swat
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@261 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminUI.php
+++ Admin/AdminUI.php
@@ -4,8 +4,6 @@
  * @copyright silverorange 2004
  */
 require_once('Swat/SwatUI.php');
-require_once('Admin/AdminActionsUIHandler.php');
-require_once('Admin/AdminActionItemUIHandler.php');
 
 /**
  * Subclass of SwatUI for use in Admin.
@@ -16,7 +14,5 @@ class AdminUI extends SwatUI {
 		parent::__construct();
 
 		$this->classmap = array('Admin' => 'Admin');
-		$this->registerHandler(new AdminActionsUIHandler());
-		$this->registerHandler(new AdminActionItemUIHandler());
 	}
 }
