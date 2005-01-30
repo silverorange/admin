a80c642cb2feda8b42315f70a26c1767610251da
Nathan Fredrickson <nathan@silverorange.com>
2005-01-30 05:01:25 +0000

    Change SwatLayout to SwatUI.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@251 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/Admin/Login.php
+++ Admin/Admin/Login.php
@@ -1,34 +1,37 @@
 <?php
 
-require_once("Admin/AdminPage.php");
-require_once('Swat/SwatLayout.php');
+require_once('Admin/AdminPage.php');
+require_once('Admin/AdminUI.php');
 
 class AdminLogin extends AdminPage {
 
-	private $layout;
+	private $ui;
 
 	public function init() {
-		$this->layout = new SwatLayout('Admin/Admin/login.xml');
+		$this->layout = 'login';
 
-		$frame = $this->layout->getWidget('frame');
+		$this->ui = new AdminUI();
+		$this->ui->loadFromXML('Admin/Admin/login.xml');
+
+		$frame = $this->ui->getWidget('frame');
 		$frame->title = $this->app->title;
 
-		$form = $this->layout->getWidget('loginform');
+		$form = $this->ui->getWidget('loginform');
 		$form->action = $this->app->uri;
 	}
 
 	public function display() {
-		$root = $this->layout->getRoot();
+		$root = $this->ui->getRoot();
 		$root->display();
 	}
 
 	public function process() {
-		$form = $this->layout->getWidget('loginform');
+		$form = $this->ui->getWidget('loginform');
 
 		if ($form->process()) {
 			if (!$form->hasErrorMessage()) {
-				$username = $this->layout->getWidget('username');
-				$password = $this->layout->getWidget('password');
+				$username = $this->ui->getWidget('username');
+				$password = $this->ui->getWidget('password');
 
 				$this->app->login($username, $password);
 				$this->app->relocate($this->app->uri);
@@ -36,9 +39,6 @@ class AdminLogin extends AdminPage {
 		}
 	}
 
-	public function getLayout() {
-		return 'login';
-	}
 }
 
 ?>
