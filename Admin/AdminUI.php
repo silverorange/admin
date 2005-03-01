b8f6d4cbc3b929d45b06040a6b955fc3152fc465
Nathan Fredrickson <nathan@silverorange.com>
2005-03-01 15:19:13 +0000

    better docs
    svn commit r319

--- Admin/AdminUI.php
+++ Admin/AdminUI.php
@@ -5,14 +5,20 @@ require_once('Swat/SwatUI.php');
 /**
  * UI manager for administrators
  *
- * Subclass of SwatUI for use with the Admin package.
+ * Subclass of {@link SwatUI} for use with the Admin package.  This can be used
+ * as a central place to add {@link SwatUI::$classmap class maps} and 
+ * {@link SwatUI::registerHandler() UI handlers} that are specific to the Admin 
+ * package.
  *
  * @package Admin
  * @copyright silverorange 2004
  */
 class AdminUI extends SwatUI {
 
-	function __construct() {
+	/**
+	 * Create a new AdminUI object
+	 */
+	public function __construct() {
 		parent::__construct();
 
 		$this->classmap = array('Admin' => 'Admin');
