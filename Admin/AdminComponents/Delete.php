9b0da5b87e5a606ede7c2f531cec6cbfbaae681c
Nathan Fredrickson <nathan@silverorange.com>
2005-03-01 15:21:05 +0000

    new admin components component
    svn commit r324

---
+++ Admin/AdminComponents/Delete.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('Admin/AdminUI.php');
+require_once('Admin/AdminDB.php');
+require_once('Admin/Admin/Confirmation.php');
+
+/**
+ * Delete confirmation page for AdminComponents
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminComponentsDelete extends AdminConfirmation {
+
+	public $items = null;
+
+	protected function displayMessage() {
+		$form = $this->ui->getWidget('confirmform');
+		$form->addHiddenField('items', $this->items);
+
+		$where_clause = 'componentid in ('.implode(', ', $this->items).')';
+		$items = AdminDB::getOptionArray($this->app->db, 'admincomponents',
+			'text:title', 'integer:componentid', '', $where_clause);
+
+		$message = $this->ui->getWidget('message');
+		$message->content = '<ul><li>';
+		$message->content .= implode('</li><li>', $items);
+		$message->content .= '</li></ul>';
+	}
+
+	protected function processResponse() {
+		$form = $this->ui->getWidget('confirmform');
+
+		if ($form->button->name == 'btn_yes') {
+
+			$sql = 'delete from admincomponents where componentid in (%s)';
+			$items = $form->getHiddenField('items');
+
+			foreach ($items as &$id)
+				$id = $this->app->db->quote($id, 'integer');
+
+			$sql = sprintf($sql, implode(',', $items));
+			$this->app->db->query($sql);
+		}
+	}
+}
+
+?>
