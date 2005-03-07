e23fa1b57ce964b097fc3d77713126cce39fd798
Nathan Fredrickson <nathan@silverorange.com>
2005-03-07 19:49:28 +0000

    Added AdminCellRendererControls
    svn commit r377

---
+++ Admin/AdminCellRendererControlDetails.php
@@ -0,0 +1,23 @@
+<?php
+require_once('Swat/SwatCellRendererControl.php');
+
+/**
+ * Details Control
+ *
+ * Convenience class for a Details button
+ *
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminCellRendererControlDetails extends SwatCellRendererControl {
+	
+	public function render($prefix) {
+		$this->width  = 28;
+		$this->height = 22;
+		$this->title = _("View Details");
+		$this->alt = _("Details");
+		$this->src = 'admin/images/b_details.png';
+	
+		parent::render($prefix);
+	}
+}
---
+++ Admin/AdminCellRendererControlTreeDetails.php
@@ -0,0 +1,34 @@
+<?php
+require_once('Swat/SwatCellRendererControl.php');
+
+/**
+ * Tree Details Control
+ *
+ * Convenience class for a tree details button
+ *
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminCellRendererControlTreeDetails extends SwatCellRendererControl {
+	
+	public $childcount = 0;
+	
+	public function render($prefix) {
+		$this->width  = 28;
+		$this->height = 22;
+
+		if ($this->childcount == 0) {
+			$this->title = _S('View Details');
+			$this->alt = _S('Details');
+			$this->src = 'admin/images/b_details.png';
+		} else {
+			$this->title = sprintf(_nS('View Details (%d sub-item)', 'View Details (%d sub-items)', 
+				$this->childcount), $this->childcount);
+
+			$this->alt = _S("Details");
+			$this->src = 'admin/images/b_details_folder.png';
+		}
+	
+		parent::render($prefix);
+	}
+}
