fe120c39c0d0f7d6c1108d9226040665f45f6c37
Nathan Fredrickson <nathan@silverorange.com>
2005-03-07 19:49:28 +0000

    Added AdminCellRendererControls
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@377 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminCellRendererControlEdit.php
@@ -0,0 +1,24 @@
+<?php
+require_once('Swat/SwatCellRendererControl.php');
+
+/**
+ * Edit Control
+ *
+ * Convenience class for an Edit button
+ *
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminCellRendererControlEdit extends SwatCellRendererControl {
+	
+	public function render($prefix) {
+		$this->width  = 28;
+		$this->height = 22;
+		$this->title = _("Edit Item");
+		$this->alt = _("Edit");
+		$this->src = 'admin/images/b_edit.png';
+	
+		parent::render($prefix);
+	}
+}
+
