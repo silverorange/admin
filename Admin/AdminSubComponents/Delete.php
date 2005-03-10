a85cc589220c25cb5584d0a0bd6a56fa92311140
Nathan Fredrickson <nathan@silverorange.com>
2005-03-10 22:29:46 +0000

    New AdminSubComponents component. This has no index since the AdminComponent/Details pages serve as the index.
    svn commit r402

---
+++ Admin/AdminSubComponents/Delete.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once('Admin/Admin/Confirmation.php');
+require_once('SwatDB/SwatDB.php');
+require_once('Admin/AdminDependency.php');
+
+/**
+ * Delete confirmation page for AdminSubComponents
+ * @package Admin
+ * @copyright silverorange 2005
+ */
+class AdminSubComponentsDelete extends AdminConfirmation {
+
+	public $items = null;
+
+	public function display() {
+		$form = $this->ui->getWidget('confirmform');
+		$form->addHiddenField('items', $this->items);
+		
+		$where_items = implode(', ', $this->items);
+		
+		$dep = new AdminDependency();
+		$dep->title = 'subcomponent';
+
+		$dep->entries = AdminDependency::queryDependencyEntries($this->app->db, 'adminsubcomponents',
+			'integer:subcomponentid', null, 'text:title', 'displayorder, title', 
+			'subcomponentid in ('.$where_items.')');
+
+		$message = $this->ui->getWidget('message');
+		$message->content = $dep->getMessage();
+		
+		if ($dep->getStatusLevelCount(AdminDependency::DELETE) == 0) {
+			$btn_yes = $this->ui->getWidget('btn_yes');
+			$btn_yes->visible = false;
+
+			$btn_no = $this->ui->getWidget('btn_no');
+			$btn_no->title = _S("Cancel");
+		}
+
+		parent::display();
+	}
+
+	protected function processResponse() {
+		$form = $this->ui->getWidget('confirmform');
+
+		if ($form->button->name == 'btn_yes') {
+
+			$sql = 'delete from adminsubcomponents where subcomponentid in (%s)';
+			$items = $form->getHiddenField('items');
+
+			foreach ($items as &$id)
+				$id = $this->app->db->quote($id, 'integer');
+
+			$sql = sprintf($sql, implode(',', $items));
+			$this->app->db->query($sql);
+
+			$this->app->addMessage(sprintf(_nS('%d sub-component has been deleted.', 
+				'%d sub-components have been deleted.', count($items)), count($items)));
+		}
+	}
+}
+
+?>
---
+++ Admin/AdminSubComponents/Order.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('Admin/Admin/Order.php');
+require_once('Admin/AdminUI.php');
+require_once('SwatDB/SwatDB.php');
+
+/**
+ * Order page for AdminSubComponents
+ * @package Admin
+ * @copyright silverorange 2005
+ */
+class AdminSubComponentsOrder extends AdminOrder {
+
+	private $parent;
+
+	public function init() {
+		parent::init();
+
+		$this->parent = SwatApplication::initVar('parent');
+		$form = $this->ui->getWidget('orderform');
+		$form->addHiddenField('parent', $this->parent);
+	}
+
+	public function loadData() {
+		$where_clause = sprintf('component = %s',
+			$this->app->db->quote($this->parent, 'integer'));
+
+		$order_list = $this->ui->getWidget('order');
+		$order_list->options = SwatDB::getOptionArray($this->app->db, 'adminsubcomponents', 
+			'title', 'subcomponentid', 'displayorder, title', $where_clause);
+
+		$sql = 'select sum(displayorder) from adminsubcomponents where '.$where_clause;
+		$sum = $this->app->db->queryOne($sql, 'integer');
+		$radio_list = $this->ui->getWidget('options');
+		$radio_list->value = ($sum == 0) ? 'auto' : 'custom';
+	}
+	
+	public function saveIndex($id, $index) {
+		SwatDB::updateColumn($this->app->db, 'adminsubcomponents', 'integer:displayorder',
+			$index, 'integer:subcomponentid', array($id));
+	}
+}
+?>
