cd5400721489f02415353bf0a22f62ddb92541fd
Nathan Fredrickson <nathan@silverorange.com>
2005-03-12 17:07:32 +0000

    Added AdminTableViewOrderableColumn, a swat orderable column with default values appropiate for an admin.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@436 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminTableViewOrderableColumn.php
@@ -0,0 +1,18 @@
+<?php
+require_once('Swat/SwatTableViewOrderableColumn.php');
+
+/**
+ * An orderable column
+ *
+ * @package Admin
+ * @copyright silverorange 2005
+ */
+class AdminTableViewOrderableColumn extends SwatTableViewOrderableColumn {
+
+	public function displayHeader() {
+		$this->href = $_GET['source'];
+		$this->unset_get_vars = array('source');
+		parent::displayHeader();
+	}
+
+}
