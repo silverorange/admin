8e35067a3f76798d3a6187bb32a6523bcdb18524
Nick Burka <nick@silverorange.com>
2005-06-08 15:07:37 +0000

    Added an entire AdminUsers section with index, edit, history, and details
    svn commit r793

---
+++ Admin/AdminUsers/Details.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('Admin/AdminUI.php');
+require_once('Admin/Admin/Index.php');
+require_once('Admin/AdminTableStore.php');
+require_once('SwatDB/SwatDB.php');
+
+/**
+ * Index page for Portfolio 
+ * @package silverorange3
+ * @copyright silverorange 2005
+ */
+class AdminUserDetails extends AdminIndex {
+
+	public function init() {
+		$this->ui = new AdminUI();
+		$this->ui->loadFromXML('Admin/AdminUsers/details.xml');
+	}
+
+	public function displayInit() {
+		$id = $this->app->initVar('id');
+
+		$row = SwatDB::queryRow($this->app->db, 'adminusers',
+			array('username','name'), 'userid' , $id);
+
+		$frame = $this->ui->getWidget('index_frame');
+		$frame->title.=' - '.$row->name;
+		
+		parent::displayInit();
+	}
+	
+	
+	protected function getTableStore() {
+		$id = $this->app->initVar('id');
+	
+		$sql = 'select logindate, loginagent, remoteip
+				from adminuserhistory
+				where usernum = %s
+				order by %s';
+
+        $sql = sprintf($sql,
+			$this->app->db->quote($id, 'integer'),
+            $this->getOrderByClause('logindate desc'));
+
+		$store = $this->app->db->query($sql, null, true, 'AdminTableStore');
+
+		return $store;
+	}	
+}
+?>
