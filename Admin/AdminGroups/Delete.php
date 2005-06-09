bc56c4187e67ff46dd72c67a77ee5e5d3c96757e
Nick Burka <nick@silverorange.com>
2005-06-09 17:59:01 +0000

    Added the AdminGroups set of tools and cleanup up AdminUsers
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@822 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminGroups/Delete.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'Admin/Admin/DBDelete.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/AdminDependency.php';
+
+/**
+ * Delete confirmation page for AdminComponents
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminGroupsDelete extends AdminDBDelete {
+
+	public function displayInit() {
+		$item_list = $this->getItemList('integer');
+		
+		$dep = new AdminDependency();
+		$dep->title = 'Admin Group';
+		$dep->status_level = AdminDependency::DELETE;
+
+		$dep->entries = AdminDependency::queryDependencyEntries($this->app->db, 'admingroups',
+			'integer:groupid', null, 'text:title', 'title', 'groupid in ('.$item_list.')');
+
+		$message = $this->ui->getWidget('confirmation_message');
+		$message->content = $dep->getMessage();
+		
+		parent::displayInit();
+	}
+
+	protected function processDBData() {
+		parent::processDBData();
+
+		$sql = 'delete from admingroups where groupid in (%s)';
+		$item_list = $this->getItemList('integer');
+		$sql = sprintf($sql, $item_list);
+		SwatDB::query($this->app->db, $sql);
+
+		$msg = new SwatMessage(sprintf(_nS("%d admin group has been deleted.", 
+			"%d admin groups have been deleted.", $this->getItemCount()), $this->getItemCount()),
+			SwatMessage::INFO);
+
+		$this->app->addMessage($msg);	
+	}
+}
+
+?>
---
+++ Admin/AdminGroups/Order.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'Admin/Admin/Order.php';
+require_once 'Admin/AdminUI.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Order page for AdminComponents
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+class AdminSectionsOrder extends AdminOrder {
+
+	private $parent;
+
+	public function init() {
+		parent::init();
+
+		$this->parent = SwatApplication::initVar('parent');
+		$form = $this->ui->getWidget('order_form');
+		$form->addHiddenField('parent', $this->parent);
+	}
+
+	public function displayInit() {
+		$frame = $this->ui->getWidget('order_frame');
+		$frame->title = _S("Order Sections");
+		parent::displayInit();
+	}
+
+	public function loadData() {
+		$order_widget = $this->ui->getWidget('order');
+		$order_widget->options = SwatDB::getOptionArray($this->app->db, 
+			'adminsections', 'title', 'sectionid', 'displayorder, title');
+
+		$sql = 'select sum(displayorder) from adminsections';
+		$sum = $this->app->db->queryOne($sql, 'integer');
+		$options_list = $this->ui->getWidget('options');
+		$options_list->value = ($sum == 0) ? 'auto' : 'custom';
+	}
+	
+	public function saveIndex($id, $index) {
+		SwatDB::updateColumn($this->app->db, 'adminsections', 'integer:displayorder',
+			$index, 'integer:sectionid', array($id));
+	}
+}
+
+?>
