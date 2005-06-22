8dbe430c7b966d16b29a43a69e52e39abb9e7952
Nick Burka <nick@silverorange.com>
2005-06-22 21:04:51 +0000

    Added a DB Order page that adds error handling - this should be used for most of our ordering pages.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@920 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/Admin/DBOrder.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('Admin/Admin/Order.php');
+
+/**
+ * DB admin ordering page
+ *
+ * An ordering page with DB error checking.
+ *
+ * @package Admin
+ * @copyright silverorange 2004
+ */
+abstract class AdminDBOrder extends AdminOrder {
+
+	protected function saveData() {
+		try {
+			$this->app->db->beginTransaction();
+			parent::saveData();
+			$this->app->db->commit();
+
+		} catch (SwatDBException $e) {
+			$this->app->db->rollback();
+
+			$msg = new SwatMessage(Admin::_('A database error has occured. The item was not saved.'), SwatMessage::ERROR);
+			$this->app->addMessage($msg);
+
+			$e->process();
+			return false;
+
+		} catch (SwatException $e) {
+			$msg = new SwatMessage(Admin::_('An error has occured. The item was not saved.'), SwatMessage::ERROR);
+			$this->app->addMessage($msg);
+
+			$e->process();
+			return false;
+		}
+		return true;
+	}
+}
+
+?>
