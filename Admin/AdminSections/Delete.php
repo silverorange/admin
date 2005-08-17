3fb182ab7b046279131f1ced34466914657240f0
Nick Burka <nick@silverorange.com>
2005-08-17 17:56:19 +0000

    Added proper dependencies to AdminSections
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@1359 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminSections/Delete.php
+++ Admin/AdminSections/Delete.php
@@ -22,6 +22,30 @@ class AdminSectionsDelete extends AdminDBDelete
 		$dep->entries = AdminDependency::queryDependencyEntries($this->app->db, 'adminsections',
 			'integer:sectionid', null, 'text:title', 'title', 'sectionid in ('.$item_list.')');
 
+		
+		$dep_components = new AdminDependency();
+		$dep_components->title = 'component';
+		$dep_components->status_level = AdminDependency::DELETE;
+		$dep_components->display_count = true;
+
+		$dep_components->entries = AdminDependency::queryDependencyEntries($this->app->db, 'admincomponents',
+			'integer:componentid', 'integer:section', 'text:title', 'title', 'section in ('.$item_list.')');
+
+		$dep->addDependency($dep_components);
+
+		
+		$dep_subcomponents = new AdminDependency();
+		$dep_subcomponents->title = 'sub-component';
+		$dep_subcomponents->status_level = AdminDependency::DELETE;
+		$dep_subcomponents->display_count = true;
+
+		$dep_subcomponents->entries = AdminDependency::queryDependencyEntries($this->app->db, 'adminsubcomponents',
+			'integer:subcomponentid', 'integer:component', 'text:title', 'title',
+			'component in (select componentid from admincomponents where section in ('.$item_list.'))');
+
+		$dep_components->addDependency($dep_subcomponents);
+
+
 		$message = $this->ui->getWidget('confirmation_message');
 		$message->content = $dep->getMessage();
 		
