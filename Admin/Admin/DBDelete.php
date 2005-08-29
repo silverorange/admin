643be65809f1180f5194c7389ebdf7cfbc7aedf8
Michael Gauthier <mike@silverorange.com>
2005-08-29 17:30:19 +0000

    Pop the navbar element added by this class's parent::init() before adding this class's navbar element.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@1595 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/Admin/DBDelete.php
+++ Admin/Admin/DBDelete.php
@@ -27,6 +27,7 @@ abstract class AdminDBDelete extends AdminDBConfirmation
 		$no_button = $this->ui->getWidget('no_button');
 		$no_button->title = Admin::_('Cancel');
 
+		$this->navbar->popElements(1);
 		$this->navbar->addElement(Admin::_('Delete'));
 	}
 
