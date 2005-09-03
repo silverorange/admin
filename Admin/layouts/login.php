981743188cb8ab95cca8d9f181b5d082725ec724
Michael Gauthier <mike@silverorange.com>
2005-09-03 00:27:02 +0000

    Display html head entries in the layouts.
    svn commit r1703

--- Admin/layouts/login.php
+++ Admin/layouts/login.php
@@ -4,6 +4,7 @@
 	<title><?=$this->title?></title>
 	<base href="<?=$this->basehref?>" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<?=$this->html_head_entries?>
 	<style type="text/css" media="all">@import "admin/admin.css";</style>
 	<style type="text/css" media="all">@import "swat/swat.css";</style>
 	<?=$this->javascript?>
