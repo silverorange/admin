dd320dc433a28dc615c009442f41fa53b4293fda
Michael Gauthier <mike@silverorange.com>
2005-09-08 17:10:13 +0000

    Groups are not actually orderable but as long as this file is here it might as well be maintained.
    - Made the order sub-component work with groups.
    - Brought up to so coding conventions.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@1860 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminGroups/Order.php
+++ Admin/AdminGroups/Order.php
@@ -5,15 +5,17 @@ require_once 'Admin/AdminUI.php';
 require_once 'SwatDB/SwatDB.php';
 
 /**
- * Order page for AdminComponents
+ * Order page for AdminGroups
+ *
  * @package Admin
  * @copyright silverorange 2004
  */
-class AdminSectionsOrder extends AdminDBOrder {
-
+class AdminGroupsOrder extends AdminDBOrder
+{
 	private $parent;
 
-	public function init() {
+	public function init()
+	{
 		parent::init();
 
 		$this->parent = SwatApplication::initVar('parent');
@@ -21,26 +23,29 @@ class AdminSectionsOrder extends AdminDBOrder {
 		$form->addHiddenField('parent', $this->parent);
 	}
 
-	public function initDisplay() {
+	public function initDisplay()
+	{
 		$frame = $this->ui->getWidget('order_frame');
 		$frame->title = Admin::_('Order Sections');
 		parent::initDisplay();
 	}
 
-	public function loadData() {
+	public function loadData()
+	{
 		$order_widget = $this->ui->getWidget('order');
 		$order_widget->options = SwatDB::getOptionArray($this->app->db, 
-			'adminsections', 'title', 'sectionid', 'displayorder, title');
+			'admingroups', 'title', 'id', 'displayorder, title');
 
-		$sql = 'select sum(displayorder) from adminsections';
+		$sql = 'select sum(displayorder) from admingroups';
 		$sum = SwatDB::queryOne($this->app->db, $sql, 'integer');
 		$options_list = $this->ui->getWidget('options');
 		$options_list->value = ($sum == 0) ? 'auto' : 'custom';
 	}
 	
-	public function saveIndex($id, $index) {
-		SwatDB::updateColumn($this->app->db, 'adminsections', 'integer:displayorder',
-			$index, 'integer:sectionid', array($id));
+	public function saveIndex($id, $index)
+	{
+		SwatDB::updateColumn($this->app->db, 'admingroups', 'integer:displayorder',
+			$index, 'integer:id', array($id));
 	}
 }
 
