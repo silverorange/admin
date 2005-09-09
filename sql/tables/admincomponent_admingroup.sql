7697e6fa14aac90f25e9776c41cf0d4e284a6fc3
Michael Gauthier <mike@silverorange.com>
2005-09-09 22:11:00 +0000

    Tidy these up a bit.
    svn commit r1894

--- sql/tables/admincomponent_admingroup.sql
+++ sql/tables/admincomponent_admingroup.sql
@@ -1,61 +1,14 @@
---
--- PostgreSQL database dump
---
-
-SET client_encoding = 'SQL_ASCII';
-SET check_function_bodies = false;
-
-SET SESSION AUTHORIZATION 'php';
-
-SET search_path = public, pg_catalog;
-
---
--- TOC entry 3 (OID 997976)
--- Name: admincomponent_admingroup; Type: TABLE; Schema: public; Owner: php
---
-
-CREATE TABLE admincomponent_admingroup (
-    component integer NOT NULL,
-    groupnum integer NOT NULL
+create table admincomponent_admingroup (
+    component integer not null
+		constraint admincomponent_admingroup_component references admincomponents(id),
+    groupnum integer not null,
+		constraint admincomponent_admingroup_groupnum references admingroups(id),
+	primary key(component, groupnum)
 );
 
-
---
--- Data for TOC entry 5 (OID 997976)
--- Name: admincomponent_admingroup; Type: TABLE DATA; Schema: public; Owner: php
---
-
-INSERT INTO admincomponent_admingroup (component, groupnum) VALUES (1, 1);
-INSERT INTO admincomponent_admingroup (component, groupnum) VALUES (4, 1);
-INSERT INTO admincomponent_admingroup (component, groupnum) VALUES (52, 1);
-INSERT INTO admincomponent_admingroup (component, groupnum) VALUES (3, 1);
-INSERT INTO admincomponent_admingroup (component, groupnum) VALUES (44, 1);
-
-
---
--- TOC entry 4 (OID 997978)
--- Name: admincomponent_admingroup_pkey; Type: CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY admincomponent_admingroup
-    ADD CONSTRAINT admincomponent_admingroup_pkey PRIMARY KEY (component, groupnum);
-
-
---
--- TOC entry 6 (OID 997980)
--- Name: $1; Type: FK CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY admincomponent_admingroup
-    ADD CONSTRAINT "$1" FOREIGN KEY (component) REFERENCES admincomponents(componentid);
-
-
---
--- TOC entry 7 (OID 997984)
--- Name: $2; Type: FK CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY admincomponent_admingroup
-    ADD CONSTRAINT "$2" FOREIGN KEY (groupnum) REFERENCES admingroups(groupid);
-
-
+-- default admincomponent_admingroup bindings
+insert into admincomponent_admingroup (component, groupnum) values (1, 1);
+insert into admincomponent_admingroup (component, groupnum) values (4, 1);
+insert into admincomponent_admingroup (component, groupnum) values (52, 1);
+insert into admincomponent_admingroup (component, groupnum) values (3, 1);
+insert into admincomponent_admingroup (component, groupnum) values (44, 1);
--- sql/tables/adminuser_admingroup.sql
+++ sql/tables/adminuser_admingroup.sql
@@ -1,66 +1,19 @@
---
--- PostgreSQL database dump
---
-
-SET client_encoding = 'SQL_ASCII';
-SET check_function_bodies = false;
-
-SET SESSION AUTHORIZATION 'php';
-
-SET search_path = public, pg_catalog;
-
---
--- TOC entry 3 (OID 997960)
--- Name: adminuser_admingroup; Type: TABLE; Schema: public; Owner: php
---
-
-CREATE TABLE adminuser_admingroup (
-    usernum integer NOT NULL,
-    groupnum integer NOT NULL
+create table adminuser_admingroup (
+    usernum integer not null
+		constraint adminuser_admingroup_usernum references adminusers(id),
+    groupnum integer not null
+		constraint adminuser_admingroup_groupnum references admingroups(id),
+	primary key(usernum, groupnum)
 );
 
-
---
--- Data for TOC entry 5 (OID 997960)
--- Name: adminuser_admingroup; Type: TABLE DATA; Schema: public; Owner: php
---
-
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (3, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (2, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (4, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (5, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (6, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (7, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (8, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (9, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (10, 1);
-INSERT INTO adminuser_admingroup (usernum, groupnum) VALUES (11, 1);
-
-
---
--- TOC entry 4 (OID 997962)
--- Name: adminuser_admingroup_pkey; Type: CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY adminuser_admingroup
-    ADD CONSTRAINT adminuser_admingroup_pkey PRIMARY KEY (usernum, groupnum);
-
-
---
--- TOC entry 6 (OID 997964)
--- Name: $1; Type: FK CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY adminuser_admingroup
-    ADD CONSTRAINT "$1" FOREIGN KEY (usernum) REFERENCES adminusers(userid);
-
-
---
--- TOC entry 7 (OID 997968)
--- Name: $2; Type: FK CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY adminuser_admingroup
-    ADD CONSTRAINT "$2" FOREIGN KEY (groupnum) REFERENCES admingroups(groupid);
-
-
+-- default adminuser_admingroup bindings
+insert into adminuser_admingroup (usernum, groupnum) values (3, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (2, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (4, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (5, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (6, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (7, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (8, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (9, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (10, 1);
+insert into adminuser_admingroup (usernum, groupnum) values (11, 1);
