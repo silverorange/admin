b3874bd1bc2f5e51e210250bcc45e6dbeba8e1bc
Michael Gauthier <mike@silverorange.com>
2005-09-09 00:51:26 +0000

    This file requires the HistoryCellRenderere as well.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@1881 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminUsers/Index.php
+++ Admin/AdminUsers/Index.php
@@ -4,7 +4,7 @@ require_once 'Admin/AdminUI.php';
 require_once 'SwatDB/SwatDB.php';
 require_once 'Admin/Admin/Index.php';
 require_once 'Admin/AdminTableStore.php';
-require_once 'Admin/AdminUsers/HistoryCellRenderer.php';
+require_once 'Admin/AdminUsers/include/HistoryCellRenderer.php';
 
 /**
  * Index page for AdminUsers component
