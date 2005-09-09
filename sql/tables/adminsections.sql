5c32ea376e884e05d148b01672b508ea87df5135
Michael Gauthier <mike@silverorange.com>
2005-09-09 22:11:00 +0000

    Tidy these up a bit.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@1894 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/adminsections.sql
+++ sql/tables/adminsections.sql
@@ -1,52 +1,18 @@
---
--- PostgreSQL database dump
---
-
-SET client_encoding = 'SQL_ASCII';
-SET check_function_bodies = false;
-
-SET SESSION AUTHORIZATION 'php';
-
-SET search_path = public, pg_catalog;
-
---
--- TOC entry 3 (OID 997918)
--- Name: adminsections; Type: TABLE; Schema: public; Owner: php
---
-
-CREATE TABLE adminsections (
-    sectionid serial NOT NULL,
-    title character varying(255),
-    description character varying(255),
-    displayorder integer DEFAULT 0,
-    "show" boolean DEFAULT true NOT NULL
+create table adminsections (
+	id serial NOT null,
+	title varchar(255),
+	description varchar(255),
+	displayorder integer default 0,
+	show boolean default true not null,
+	primary key(id)
 );
 
+-- default adminsections
+insert into adminsections (id, title, description, displayorder, show)
+	values (11, 'Settings', null, 0, true);
 
---
--- Data for TOC entry 6 (OID 997918)
--- Name: adminsections; Type: TABLE DATA; Schema: public; Owner: php
---
+insert into adminsections (id, title, description, displayorder, show)
+	values (1, 'Advanced', 'This is a section.', 10, true);
 
-INSERT INTO adminsections (sectionid, title, description, displayorder, "show") VALUES (11, 'Settings', NULL, 0, true);
-INSERT INTO adminsections (sectionid, title, description, displayorder, "show") VALUES (1, 'Advanced', 'This is a section.', 10, true);
-INSERT INTO adminsections (sectionid, title, description, displayorder, "show") VALUES (2, 'Site Content', NULL, 0, true);
-
-
---
--- TOC entry 5 (OID 997923)
--- Name: adminsections_pkey; Type: CONSTRAINT; Schema: public; Owner: php
---
-
-ALTER TABLE ONLY adminsections
-    ADD CONSTRAINT adminsections_pkey PRIMARY KEY (sectionid);
-
-
---
--- TOC entry 4 (OID 997916)
--- Name: adminsections_sectionid_seq; Type: SEQUENCE SET; Schema: public; Owner: php
---
-
-SELECT pg_catalog.setval('adminsections_sectionid_seq', 11, true);
-
-
+insert into adminsections (id, title, description, displayorder, show)
+	values (2, 'Site Content', null, 0, true);
--- sql/tables/adminuserhistory.sql
+++ sql/tables/adminuserhistory.sql
@@ -1,8 +1,9 @@
 create table adminuserhistory (
-        historyid serial,
-        usernum int not null constraint fk_adminuserhistory_usernum references adminusers(userid),
-        logindate timestamp,
-        loginagent varchar(255),
-        remoteip varchar(15),
-        primary key(historyid)
-)
+	id serial,
+	usernum integer not null
+		constraint fk_adminuserhistory_usernum references adminusers(id),
+	logindate timestamp,
+	loginagent varchar(255),
+	remoteip varchar(15),
+	primary key(id)
+);
