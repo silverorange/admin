6e06a4882143614608dccf584acfabe3ee36d1b3
Michael Gauthier <mike@silverorange.com>
2005-09-20 18:16:06 +0000

    Added a special navbar that can display some special entries as <h1> elements.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@2011 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminImportantNavBarEntry.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Swat/SwatNavBarEntry.php';
+
+/**
+ * An important entry for the admin navigation tool
+ *
+ * @package   Admin
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ *
+ * @see SwatNavBarEntry
+ */
+class AdminImportantNavBarEntry extends SwatNavBarEntry
+{
+}
+
+?>
