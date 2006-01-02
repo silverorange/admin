9109d8998744b5d86488c06a214a3b79cfcadaf6
Michael Gauthier <mike@silverorange.com>
2006-01-02 17:00:16 +0000

    Updates to admin dependency. - Allows better translations and pluralizations through the use of string   getting methods. Sub-classes can use whatever strings they want. - Updated and improved documentation. - Made base class abstract and made two sub-classes for lists and summary   dependency display.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@2840 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminListDependency.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once 'Admin/AdminDependency.php';
+
+/**
+ * A dependency that displays its dependencies as a list
+ *
+ * @package   Admin
+ * @copyright 2005 silverorange
+ */
+class AdminListDependency extends AdminDependency
+{
+	/**
+	 * Gets the text for a dependency list for this dependency
+	 *
+	 * Sub-classes may override this method to have more descriptive or
+	 * meaningful text.
+	 *
+	 * @param integer $count the number of dependencies in the list.
+	 *
+	 * @return string the text for a dependency list for this dependency.
+	 */
+	protected function getDependencyText($count)
+	{
+		if ($this->title === null) {
+			$message = Admin::ngettext('Dependent item:',
+				'Dependent items:', $count);
+		} else {
+			$message = Admin::ngettext('Dependent %s:',
+				'Dependent %ss:', $count);
+
+			$message = sprintf($message, $this->title);
+		}
+		return $message;
+	}
+
+	/**
+	 * Displays a list of the dependency entries of this dependency for a given
+	 * parent at a given status level
+	 *
+	 * @param integer $parent the id of the parent to display the list for.
+	 * @param integer $status_level the status level to display the list for.
+	 */
+	protected function displayDependencies($parent, $status_level)
+	{
+		$count = 0;
+		foreach ($this->entries as $entry)
+			if ($entry->parent == $parent &&
+				$entry->status_level == $status_level)
+				$count++;
+
+		$first = true;
+
+		foreach ($this->entries as $entry) {
+			if ($entry->parent == $parent &&
+				$entry->status_level == $status_level) {
+
+				if ($first) {
+					echo '<br />';
+					echo $this->getDependencyText($count);
+					echo '<ul>';
+					$first = false;
+				}
+
+				echo '<li>'.$entry->title;
+
+				foreach ($this->dependencies as $dep)
+					$dep->displayDependencies($entry->id, $status_level);
+
+				echo '</li>';
+			}
+		}
+		if ($count > 0)
+			echo '</ul>';
+	}
+}
+
+?>
---
+++ Admin/AdminSummaryDependency.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once 'Admin/AdminDependency.php';
+
+/**
+ * A dependency that displays its dependencies as a one line summary
+ *
+ * @package   Admin
+ * @copyright 2005 silverorange
+ */
+class AdminSummaryDependency extends AdminDependency
+{
+	/**
+	 * Gets the text for a dependency list summary for this dependency
+	 *
+	 * Sub-classes may override this method to have more descriptive or
+	 * meaningful text.
+	 *
+	 * @param integer $count the number of dependencies in the list.
+	 *
+	 * @return string the text for a dependency list summary for this
+	 *                 dependency.
+	 */
+	protected function getDependencyText($count)
+	{
+		if ($this->title === null) {
+			$message = Admin::ngettext('%d dependent item',
+				'%d dependent items', $count);
+
+			$message = sprintf($message, $count);
+		} else {
+			$message = Admin::ngettext('%d dependent %s',
+				'%d dependent %ss', $count);
+
+			$message = sprintf($message, $count, $this->title);
+		}
+		return $message;
+	}
+
+	/**
+	 * Displays a summary of the dependency entries for the given parent
+	 * at a given status level
+	 *
+	 * @param integer $parent the id of the parent to display the summary for.
+	 * @param integer $status_level the status level to display the summary for.
+	 */
+	protected function displayDependencies($parent, $status_level)
+	{
+		$count = 0;
+		foreach ($this->entries as $entry)
+			if ($entry->parent == $parent &&
+				$entry->status_level == $status_level)
+				$count++;
+
+		if ($count != 0) {
+			// TODO: class here
+			echo ' <span style="color:#888;">(';
+			echo $this->getDependencyText($count);
+		}
+
+		foreach ($this->entries as $entry)
+			if ($entry->parent == $parent &&
+				$entry->status_level == $status_level)
+				foreach ($this->dependencies as $dep)
+					$dep->displayDependencies($entry->id, $status_level);
+
+		if ($count != 0)
+			echo ')</span>';
+	}
+}
+
+?>
