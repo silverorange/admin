fa2e9943e5b694c15c1655b5971111b3c5f94f56
Nathan Fredrickson <nathan@silverorange.com>
2006-01-18 01:00:45 +0000

    New widget to display static informational notes on admin pages. Steve, please review/fix the CSS I copied from SwatMessageDisplay. Test page: http://localhost:8080/veseys2/work-nrf/www/admin/Promotions/Details?id=MANTIS
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@3007 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminNote.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once 'Swat/SwatContentBlock.php';
+require_once 'Swat/SwatHtmlTag.php';
+
+/**
+ * A widget to display a formatted note in the widget tree
+ *
+ * @package   Admin
+ * @copyright 2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class AdminNote extends SwatContentBlock
+{
+	/**
+	 * User visible title of this widget
+	 *
+	 * @var string
+	 */
+	public $title = '';
+
+	/**
+	 * Creates a new note
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+		$this->addStyleSheet('admin/styles/admin-note.css');
+	}
+
+	/**
+	 * Displays this content
+	 *
+	 * Merely performs an echo of the content.
+	 */
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		$div = new SwatHtmlTag('div');
+		$div->class = 'admin-note';
+		$div->open();
+
+		$header_tag = new SwatHtmlTag('h3');
+		$header_tag->class = 'admin-note-title';
+		$header_tag->setContent($this->title);
+		$header_tag->display();
+
+		if ($this->content !== null) {
+			$content_div = new SwatHtmlTag('div');
+			$content_div->class = 'admin-note-content';
+			$content_div->setContent($this->content, $this->content_type);
+			$content_div->display();
+		}
+
+		$div->close();
+	}
+}
+
+?>
