3bf4e4f9f9bc6d9188af42dbff91928b8243c3c5
Steven Garrity <steven@silverorange.com>
2006-01-23 20:07:03 +0000

    New AdminTitleLinkCellRenderer for the primary title row in Admin index pages
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@3057 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Admin/AdminTitleLinkCellRenderer.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Swat/SwatLinkCellRenderer.php';
+require_once 'Swat/SwatHtmlTag.php';
+
+/**
+ * A title link cell renderer for Admin index pages
+ *
+ * Links in the cell renderer are styled as block-level elements, 
+ * so other cell renderers in the same table cell may cause layout issues.
+ *
+ * @package   Admin
+ * @copyright 2006 silverorange
+ */
+class AdminTitleLinkCellRenderer extends SwatLinkCellRenderer
+{
+
+	/**
+	 * Gets TD-tag attributes
+	 *
+	 * Overridden here to provide a custom CSS hook for admin title link cells.
+	 *
+	 * @return array an array of attributes to apply to the TD tag of this cell
+	 *                renderer.
+	 */
+	public function getTdAttributes()
+	{
+		return array('class' => 'admin-title-link-cell-renderer');
+	}
+}
+
+?>
