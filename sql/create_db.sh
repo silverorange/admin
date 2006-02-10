3d94389b711ab26b0fb3179594f8b827116e2f49
David Peck <dave@silverorange.com>
2006-02-10 16:45:32 +0000

    make this one match veseys2
    svn commit r3384

--- sql/create_db.sh
+++ sql/create_db.sh
@@ -6,41 +6,42 @@
 # createdb -U php <database>
 # createlang -U postgres plpgsql <database>
 
+SOURCE="/so/packages/admin/work-dave/sql"
+
 #
 # TABLES
 #
-ADMINDIR="/so/packages/admin/work-dave/sql"
 
-cat ${ADMINDIR}/tables/adminusers.sql > ./create_db.sql
-cat ${ADMINDIR}/tables/admingroups.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/adminsections.sql >> ./create_db.sql
-cat ${ADMINDIR}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/admincomponents.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/adminsubcomponents.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/adminuserhistory.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/adminhelp.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/adminuser_admingroup.sql >> ./create_db.sql
-cat ${ADMINDIR}/tables/admincomponent_admingroup.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminusers.sql > ./create_db.sql
+cat ${SOURCE}/tables/admingroups.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminsections.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
+cat ${SOURCE}/tables/admincomponents.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminsubcomponents.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminuserhistory.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminhelp.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminuser_admingroup.sql >> ./create_db.sql
+cat ${SOURCE}/tables/admincomponent_admingroup.sql >> ./create_db.sql
 
 #
 # VIEWS
 #
 
-cat ${ADMINDIR}/views/view_adminuser_lastlogin.sql >> ./create_db.sql
+cat ${SOURCE}/views/view_adminuser_lastlogin.sql >> ./create_db.sql
 
 #
 # STORED PROCEDURES
 #
 
-cat ${ADMINDIR}/stored_procedures/sp_admin_menu.sql >> ./create_db.sql
+cat ${SOURCE}/stored_procedures/sp_admin_menu.sql >> ./create_db.sql
 
 #
 # TRIGGERS
 #
 
-cat ${ADMINDIR}/triggers/tr_adminsections_delete.sql >> ./create_db.sql
-cat ${ADMINDIR}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
-cat ${ADMINDIR}/triggers/tr_admingroups_delete.sql >> ./create_db.sql
-cat ${ADMINDIR}/triggers/tr_adminuserhistory_insert.sql >> ./create_db.sql
-cat ${ADMINDIR}/triggers/tr_adminusers_delete.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_adminsections_delete.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_admingroups_delete.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_adminuserhistory_insert.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_adminusers_delete.sql >> ./create_db.sql
 
