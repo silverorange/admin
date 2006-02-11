f5dd65545252f9663be514bbdee4f7577270170e
David Peck <dave@silverorange.com>
2006-02-11 18:34:06 +0000

    handy reset script, add full paths to create script
    svn commit r3395

--- sql/create_db.sh
+++ sql/create_db.sh
@@ -12,35 +12,35 @@ SOURCE="/so/packages/admin/work-dave/sql"
 # TABLES
 #
 
-cat ${SOURCE}/tables/adminusers.sql > ./create_db.sql
-cat ${SOURCE}/tables/admingroups.sql >> ./create_db.sql
-cat ${SOURCE}/tables/adminsections.sql >> ./create_db.sql
-cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
-cat ${SOURCE}/tables/admincomponents.sql >> ./create_db.sql
-cat ${SOURCE}/tables/adminsubcomponents.sql >> ./create_db.sql
-cat ${SOURCE}/tables/adminuserhistory.sql >> ./create_db.sql
-cat ${SOURCE}/tables/adminuser_admingroup.sql >> ./create_db.sql
-cat ${SOURCE}/tables/admincomponent_admingroup.sql >> ./create_db.sql
+cat ${SOURCE}/tables/adminusers.sql > ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/admingroups.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/adminsections.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/admincomponents.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/adminsubcomponents.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/adminuserhistory.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/adminuser_admingroup.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/tables/admincomponent_admingroup.sql >> ${SOURCE}/create_db.sql
 
 #
 # VIEWS
 #
 
-cat ${SOURCE}/views/view_adminuser_lastlogin.sql >> ./create_db.sql
+cat ${SOURCE}/views/view_adminuser_lastlogin.sql >> ${SOURCE}/create_db.sql
 
 #
 # STORED PROCEDURES
 #
 
-cat ${SOURCE}/stored_procedures/sp_admin_menu.sql >> ./create_db.sql
+cat ${SOURCE}/stored_procedures/sp_admin_menu.sql >> ${SOURCE}/create_db.sql
 
 #
 # TRIGGERS
 #
 
-cat ${SOURCE}/triggers/tr_adminsections_delete.sql >> ./create_db.sql
-cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ./create_db.sql
-cat ${SOURCE}/triggers/tr_admingroups_delete.sql >> ./create_db.sql
-cat ${SOURCE}/triggers/tr_adminuserhistory_insert.sql >> ./create_db.sql
-cat ${SOURCE}/triggers/tr_adminusers_delete.sql >> ./create_db.sql
+cat ${SOURCE}/triggers/tr_adminsections_delete.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/triggers/tr_admincomponents_delete.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/triggers/tr_admingroups_delete.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/triggers/tr_adminuserhistory_insert.sql >> ${SOURCE}/create_db.sql
+cat ${SOURCE}/triggers/tr_adminusers_delete.sql >> ${SOURCE}/create_db.sql
 
