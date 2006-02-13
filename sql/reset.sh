9d5a9123a987c3f82db299717d944a943492f140
David Peck <dave@silverorange.com>
2006-02-13 21:00:24 +0000

    use a better db
    svn commit r3486

--- sql/reset.sh
+++ sql/reset.sh
@@ -5,11 +5,11 @@ SOURCE="/so/packages/admin/work-dave/sql"
 clear
 ${SOURCE}/create_db.sh
 sleep 1
-dropdb -U php mytest
+dropdb -U php veseys2_import
 sleep 1
-createdb -U php mytest
-createlang -U postgres plpgsql mytest
+createdb -U php veseys2_import
+createlang -U postgres plpgsql veseys2_import
 sleep 1
-psql -U php -f ${SOURCE}/create_db.sql mytest
+psql -U php -f ${SOURCE}/create_db.sql veseys2_import
 rm ${SOURCE}/create_db.sql
 
