1b2a541ffe11230474ccebc218de88ab0cba8b19
David Peck <dave@silverorange.com>
2006-02-15 21:06:56 +0000

    use UTF8 encoding to create the DB
    svn commit r3556

--- sql/reset.sh
+++ sql/reset.sh
@@ -7,7 +7,7 @@ ${SOURCE}/create_db.sh
 sleep 1
 dropdb -U php veseys2_import
 sleep 1
-createdb -U php veseys2_import
+createdb -U php -E UTF8 veseys2_import
 createlang -U postgres plpgsql veseys2_import
 sleep 1
 psql -U php -f ${SOURCE}/create_db.sql veseys2_import
