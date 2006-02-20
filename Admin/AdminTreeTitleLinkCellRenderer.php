4b4730bdf14e1f3eeb48eaef2b364600c5f14811
Nathan Fredrickson <nathan@silverorange.com>
2006-02-20 16:49:30 +0000

    Underscorify
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@3642 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminTreeTitleLinkCellRenderer.php
+++ Admin/AdminTreeTitleLinkCellRenderer.php
@@ -10,7 +10,7 @@ require_once 'Admin/AdminTitleLinkCellRenderer.php';
  */
 class AdminTreeTitleLinkCellRenderer extends AdminTitleLinkCellRenderer
 {
-	public $childcount = 0;
+	public $child_count = 0;
 	public $base_stock_id = 'document';
 
 	public function render()
@@ -21,7 +21,7 @@ class AdminTreeTitleLinkCellRenderer extends AdminTitleLinkCellRenderer
 
 	protected function setStockType()
 	{
-		if (intval($this->childcount) > 0)
+		if (intval($this->child_count) > 0)
 			$this->setFromStock($this->base_stock_id.'-with-contents');
 		else
 			$this->setFromStock($this->base_stock_id);
@@ -29,11 +29,11 @@ class AdminTreeTitleLinkCellRenderer extends AdminTitleLinkCellRenderer
 
 	protected function getTitle()
 	{
-		if (intval($this->childcount) == 0)
+		if (intval($this->child_count) == 0)
 			return Admin::gettext('no sub-items');
 
 		return sprintf(Admin::ngettext('%d sub-item', '%d sub-items', 
-			$this->childcount), $this->childcount);
+			$this->child_count), $this->child_count);
 	}
 }
 
