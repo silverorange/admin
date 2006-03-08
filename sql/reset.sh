ad32fec01fc02ab9475da65e0a233d2408051dfc
David Peck <dave@silverorange.com>
2006-03-08 19:49:41 +0000

    put the db name in a var for easy switching
    svn commit r4052

--- sql/reset.sh
+++ sql/reset.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 SOURCE="/so/packages/admin/work-dave/sql"
+DB="veseys2_import"
 
 clear
 ${SOURCE}/create_db.sh
 sleep 1
-dropdb -U php veseys2_import
+dropdb -U php $DB
 sleep 1
-createdb -U php -E UTF8 veseys2_import
-createlang -U postgres plpgsql veseys2_import
+createdb -U php -E UTF8 $DB
+createlang -U postgres plpgsql $DB
 sleep 1
-psql -U php -f ${SOURCE}/create_db.sql veseys2_import
+psql -U php -f ${SOURCE}/create_db.sql $DB
 rm ${SOURCE}/create_db.sql
 
