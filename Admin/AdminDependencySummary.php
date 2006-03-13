6f4cee9db2f13568f21332211d3e7c112c7ae52c
Michael Gauthier <mike@silverorange.com>
2006-03-13 19:32:56 +0000

    Refactoring of admin dependencies to do two things: - Make it so you don't query rows unnecessarily for summary dependencies.   (saves space and time). - Make convenience methods automatically wrap results as objects rather than   doing this in a processing method.   (saves more space and time).
    svn commit r4166

---
+++ Admin/AdminDependencySummary.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'Admin/AdminDependencyItem.php';
+
+/**
+ * A dependency summary that contains a count of items
+ *
+ * @package   Admin
+ * @copyright 2006 silverorange
+ */
+class AdminDependencySummary extends AdminDependencyItem
+{
+	/**
+	 * Creates a new AdminDependencySummary
+	 *
+	 * This constructor enables the entry to be used in a MDB2 data wrapper
+	 * to automatically create objects from a result set.
+	 *
+	 * @param mixed $data the MDB2 row containing the data for this summary
+	 *                     object.
+	 */
+	public function __construct($data = null)
+	{
+		if ($data !== null) {
+			$this->count = $data->count;
+			$this->parent = $data->parent;
+			$this->status_level = $data->status_level;
+		}
+	}
+
+	/**
+	 * The number of items in this summary
+	 *
+	 * @var integer
+	 */
+	public $count;
+}
+
+?>
