987d31738722f86af79c897b20409b79f2b04150
Michael Gauthier <mike@silverorange.com>
2006-03-29 20:01:07 +0000

    Use HTTP POST for logging out of the admin. This is the correct way to do things.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@4414 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/components/AdminSite/Logout.php
+++ Admin/components/AdminSite/Logout.php
@@ -1,21 +1,29 @@
 <?php
 
 require_once 'Admin/pages/AdminPage.php';
+require_once 'Admin/exceptions/AdminNotFoundException.php';
 
 /**
  * Very simple administrator logout page
- * @package Admin
- * @copyright silverorange 2004
+ *
+ * @package   Admin
+ * @copyright 2004-2006 silverorange
  */
 class AdminSiteLogout extends AdminPage
 {
 	// process phase
-	// {{{ protected functipn processInternal()
+	// {{{ protected function processInternal()
 
 	protected function processInternal()
 	{
-		$this->app->session->logout();
-		$this->app->relocate($this->app->getBaseHref());
+		$this->logout_form->process();
+
+		if ($this->logout_form->isProcessed()) {
+			$this->app->session->logout();
+			$this->app->relocate($this->app->getBaseHref());
+		} else {
+			throw new AdminNotFoundException();
+		}
 	}
 
 	// }}}
