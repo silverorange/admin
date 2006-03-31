32e4efcbc609f403db099f5211db03ce63641370
Michael Gauthier <mike@silverorange.com>
2006-03-31 20:18:19 +0000

    Bring coding standards to 2006.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@4451 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminPagination.php
+++ Admin/AdminPagination.php
@@ -1,21 +1,21 @@
 <?php
 
-require_once('Swat/SwatPagination.php');
+require_once 'Swat/SwatPagination.php';
 
 /**
  * An orderable column
  *
- * @package Admin
- * @copyright silverorange 2005
+ * @package   Admin
+ * @copyright 2005-2006 silverorange
  */
-class AdminPagination extends SwatPagination {
-
-	public function display() {
+class AdminPagination extends SwatPagination
+{
+	public function display()
+	{
 		$this->href = $_GET['source'];
 		$this->unset_get_vars = array('source');
 		parent::display();
 	}
-
 }
 
 ?>
