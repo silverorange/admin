3cfa784560e17af2125ad5a1a7d95c9867c0d640
Nick Burka <nick@silverorange.com>
2006-04-13 22:10:09 +0000

    Update structure
    svn commit r4696

--- Admin/AdminPagination.php
+++ Admin/AdminPagination.php
@@ -3,19 +3,79 @@
 require_once 'Swat/SwatPagination.php';
 
 /**
- * An orderable column
+ * A widget to allow navigation between paginated data.
+ * Preserves GET var information
  *
- * @package   Admin
- * @copyright 2005-2006 silverorange
+ * @package Admin
+ * @copyright silverorange 2004
  */
 class AdminPagination extends SwatPagination
 {
-	public function display()
+	// {{{ public properties
+
+	/**
+	 * HTTP GET vars to clobber
+	 *
+	 * An array of GET variable names to unset before rebuilding a new link.
+	 *
+	 * @var array
+	 */
+	public $unset_get_vars = array();
+
+	// }}}
+	// {{{ public function process()
+
+	/**
+	 * Processes this pagination widget
+	 *
+	 * Sets the current_page and current_record properties.
+	 */
+	public function process()
 	{
-		$this->href = $_GET['source'];
-		$this->unset_get_vars = array('source');
-		parent::display();
+		parent::process();
+
+		if (array_key_exists($this->id, $_GET))
+			$this->current_page = $_GET[$this->id];
+
+		$this->current_record = $this->current_page * $this->page_size;
 	}
+
+	// }}}
+	// {{{ protected function getLink()
+
+	/**
+	 * Gets the base link for all page links
+	 *
+	 * This removes all unwanted elements from the get variables and adds
+	 * all the wanted ones back into an acceptable url string.
+	 *
+	 * @return string the base link for all pages with cleaned get variables.
+	 */
+	protected function getLink()
+	{
+		//$vars = array_diff_key($_GET, array_flip($this->unset_get_vars));
+		$vars = $_GET;
+
+		$this->unset_get_vars[] = $this->id;
+
+		foreach($vars as $name => $value)
+			if (in_array($name, $this->unset_get_vars))
+				unset($vars[$name]);
+
+		if ($this->link === null)
+			$link = '?';
+		else
+			$link = $this->link.'?';
+
+		foreach($vars as $name => $value)
+			$link .= $name.'='.urlencode($value).'&';
+
+		$link.= urlencode($this->id).'=%s';
+
+		return $link;
+	}
+
+	// }}}
 }
 
 ?>
