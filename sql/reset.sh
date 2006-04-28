aca8e18a06fa766368ef4857c3e51005d038e71c
David Peck <dave@silverorange.com>
2006-04-28 14:41:57 +0000

    make this a bit more user friendly, don't hard code db
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@5058 94490950-a86c-4493-9ccb-530f72933e47

--- sql/reset.sh
+++ sql/reset.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
 SOURCE="/so/packages/admin/work-dave/sql"
-DB="veseys2_import"
+
+if [ -z $1 ]; then
+	echo "need destination db name"
+	exit 0
+else
+	DB=$1
+fi
 
 clear
+echo "Database: $1"
+echo
+echo
+
+echo "Generating the SQL"
 ${SOURCE}/create_db.sh
 sleep 1
+
+echo "Dropping the old database"
 dropdb -U php $DB
 sleep 1
+
+echo "Creating the new database"
 createdb -U php -E UTF8 $DB
 createlang -U postgres plpgsql $DB
 sleep 1
+
+echo "Creating the generic admin"
 psql -U php -f ${SOURCE}/create_db.sql $DB
 rm ${SOURCE}/create_db.sql
 
