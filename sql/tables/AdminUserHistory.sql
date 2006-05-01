299423f078949ecae19cf5ddedce0be2ceeb8c72
Isaac Grant <isaac@silverorange.com>
2006-05-01 15:02:20 +0000

    underscorify a couple of fields
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@5122 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/AdminUserHistory.sql
+++ sql/tables/AdminUserHistory.sql
@@ -2,9 +2,9 @@ create table AdminUserHistory (
 	id serial,
 	usernum integer not null
 		constraint fk_adminuserhistory_usernum references AdminUser(id),
-	logindate timestamp,
-	loginagent varchar(255),
-	remoteip varchar(15),
+	login_date timestamp,
+	login_agent varchar(255),
+	remote_ip varchar(15),
 	primary key(id)
 );
 
