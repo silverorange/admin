2850cd9e2b9a7303a4893e0c373afc75ea5abd96
Steven Garrity <steven@silverorange.com>
2006-05-25 17:41:08 +0000

    Use a "." rather than empty "" href for root navbar entry link
    svn commit r6110

--- Admin/AdminApplication.php
+++ Admin/AdminApplication.php
@@ -190,7 +190,7 @@ class AdminApplication extends SiteApplication
 			$page->source = $request->source;
 			$page->component = $request->component;
 			$page->subcomponent = $request->subcomponent;
-			$page->navbar->addEntry(new AdminImportantNavBarEntry($this->title, ''));
+			$page->navbar->addEntry(new AdminImportantNavBarEntry($this->title, '.'));
 			$page->navbar->addEntry(new SwatNavBarEntry($request->title, 
 				($request->subcomponent == 'Index') ? null : $request->component));
 		}
