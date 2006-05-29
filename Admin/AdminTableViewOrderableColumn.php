ea3cd31592c31c549ddba819bbcbcfe81075e157
Michael Gauthier <mike@silverorange.com>
2006-05-29 19:57:19 +0000

    Use coding conventions.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@6300 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminTableViewOrderableColumn.php
+++ Admin/AdminTableViewOrderableColumn.php
@@ -5,17 +5,17 @@ require_once 'Swat/SwatTableViewOrderableColumn.php';
 /**
  * An orderable column
  *
- * @package Admin
- * @copyright silverorange 2005
+ * @package   Admin
+ * @copyright 2005 silverorange
  */
-class AdminTableViewOrderableColumn extends SwatTableViewOrderableColumn {
-
-	public function displayHeader() {
+class AdminTableViewOrderableColumn extends SwatTableViewOrderableColumn
+{
+	public function displayHeader()
+	{
 		$this->link = $_GET['source'];
 		$this->unset_get_vars = array('source');
 		parent::displayHeader();
 	}
-
 }
 
 ?>
