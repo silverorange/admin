9b8f6737d7ba67c35a46f5ded7acd79d6f48d638
David Peck <dave@silverorange.com>
2006-08-15 15:24:04 +0000

    simplify create scripts - move to text lists and a single shell script
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@9620 94490950-a86c-4493-9ccb-530f72933e47

--- sql/create_sql.sh
+++ sql/create_sql.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-SRC="/so/packages/admin/work-dave/sql"
+WHOAMI=`whoami`
+SRC="/so/packages/admin/work-${WHOAMI}/sql"
+DST="${SRC}/statements.sql"
 
 if [ -z $1 ]; then
 	echo "need destination db name"
@@ -14,10 +16,6 @@ echo "Database: $1"
 echo
 echo
 
-echo "Generating the SQL"
-${SRC}/generate_sql.sh
-sleep 1
-
 echo "Dropping the old database"
 dropdb -U php $DB
 sleep 1
@@ -27,7 +25,26 @@ createdb -U php -E UTF8 $DB
 createlang -U postgres plpgsql $DB
 sleep 1
 
+# Create an empty site.
+echo "" > $DST
+
+cat ${SRC}/tables/tables.txt | while read LINE; do
+	cat ${SRC}/tables/${LINE} >> $DST
+done
+
+cat ${SRC}/functions/functions.txt | while read LINE; do
+	cat ${SRC}/functions/${LINE} >> $DST
+done
+
+cat ${SRC}/views/views.txt | while read LINE; do
+	cat ${SRC}/views/${LINE} >> $DST
+done
+
+cat ${SRC}/triggers/triggers.txt | while read LINE; do
+	cat ${SRC}/triggers/${LINE} >> $DST
+done
+
 echo "Creating the generic admin"
-psql -U php -f ${SRC}/statements.sql $DB
-rm ${SRC}/statements.sql
+psql -U php -f $DST $DB
+rm $DST
 
