7f7cfa96e4cd1ea7d26d781b2db2f902310cbfb0
David Peck <dave@silverorange.com>
2006-08-15 16:25:11 +0000

    make admin stand alone - projects should create databases, not packages, even though this one will almost always be run first
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@9629 94490950-a86c-4493-9ccb-530f72933e47

--- sql/create_sql.sh
+++ sql/create_sql.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 WHOAMI=`whoami`
-SRC="/so/packages/admin/work-${WHOAMI}/sql"
-DST="${SRC}/statements.sql"
+WD="/so/packages/admin/work-${WHOAMI}/sql"
+DST="${WD}/statements.sql"
 
 if [ -z $1 ]; then
 	echo "need destination db name"
@@ -11,37 +11,23 @@ else
 	DB=$1
 fi
 
-clear
-echo "Database: $1"
-echo
-echo
-
-echo "Dropping the old database"
-dropdb -U php $DB
-sleep 1
-
-echo "Creating the new database"
-createdb -U php -E UTF8 $DB
-createlang -U postgres plpgsql $DB
-sleep 1
-
 # Create an empty site.
 echo "" > $DST
 
-cat ${SRC}/tables/tables.txt | while read LINE; do
-	cat ${SRC}/tables/${LINE} >> $DST
+cat ${WD}/tables/tables.txt | while read LINE; do
+	cat ${WD}/tables/${LINE} >> $DST
 done
 
-cat ${SRC}/functions/functions.txt | while read LINE; do
-	cat ${SRC}/functions/${LINE} >> $DST
+cat ${WD}/functions/functions.txt | while read LINE; do
+	cat ${WD}/functions/${LINE} >> $DST
 done
 
-cat ${SRC}/views/views.txt | while read LINE; do
-	cat ${SRC}/views/${LINE} >> $DST
+cat ${WD}/views/views.txt | while read LINE; do
+	cat ${WD}/views/${LINE} >> $DST
 done
 
-cat ${SRC}/triggers/triggers.txt | while read LINE; do
-	cat ${SRC}/triggers/${LINE} >> $DST
+cat ${WD}/triggers/triggers.txt | while read LINE; do
+	cat ${WD}/triggers/${LINE} >> $DST
 done
 
 echo "Creating the generic admin"
