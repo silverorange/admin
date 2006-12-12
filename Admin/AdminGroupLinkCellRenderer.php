d5dfbe08234360e16c1272dc36fda6d7af910b74
Isaac Grant <isaac@silverorange.com>
2006-12-12 17:49:41 +0000

    clean up some bitrot:
    - SwatContentBlock doesn't need to be inside a SwatDisplayableContainer for confirmation.xml
    - addClass hasn't existed in a long time - use getCSSClassNames to add the class to the display of 
    AdminGroupLinkCellRenderer - fixing #398
    
    
    
    svn commit r13812

--- Admin/AdminGroupLinkCellRenderer.php
+++ Admin/AdminGroupLinkCellRenderer.php
@@ -16,10 +16,31 @@ class AdminGroupLinkCellRenderer extends SwatLinkCellRenderer
 	{
 		parent::__construct();
 
-		$this->addStyleSheet('packages/admin/styles/admin-group-link-cell-renderer.css',
+		$this->addStyleSheet(
+			'packages/admin/styles/admin-group-link-cell-renderer.css',
 			Admin::PACKAGE_ID);
+	}
 
-		$this->class = 'admin-group-link-cell-renderer';
+	// }}}
+	// {{{ protected function getCSSClassNames()
+
+	/**
+	 * Gets the array of CSS classes that are applied to this user-interface
+	 * object
+	 *
+	 * User-interface objects aggregate the list of user-specified classes and
+	 * may add static CSS classes of their own in this method.
+	 *
+	 * @return array the array of CSS classes that are applied to this
+	 *                user-interface object.
+	 *
+	 * @see SwatUIObject::getCSSClassString()
+	 */
+	protected function getCSSClassNames()
+	{
+		$classes = array('admin-group-link-cell-renderer');
+
+		return array_merge($classes, $this->classes);
 	}
 
 	// }}}
