db23cd2c3d878f1f935ea2d92bbc85e880b08ed0
Isaac Grant <isaac@silverorange.com>
2007-01-09 14:49:23 +0000

    you can't implode an ArrayObject
    svn commit r14235

--- Admin/layouts/xhtml/default.php
+++ Admin/layouts/xhtml/default.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<?=$this->html_head_entries?>
 </head>
-<body class="<?= implode(' ', $this->body_classes) ?>">
+<body class="<?= implode(' ', $this->body_classes->getArrayCopy()) ?>">
 
 <div id="admin-wrapper">
 
