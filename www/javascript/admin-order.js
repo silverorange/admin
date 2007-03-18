9461748e90143e95c9b6ca3edee7432e57cc3452
Michael Gauthier <mike@silverorange.com>
2007-03-18 03:48:46 +0000

    Greatly simplify and anhance the admin order JavaScript by subscribing to the custom order changed event on the change order.
    svn commit r16009

--- www/javascript/admin-order.js
+++ www/javascript/admin-order.js
@@ -1,6 +1,6 @@
 /**
- * Adds mousedown event handlers to the items of a SwatChangeOrder that
- * checks a radio button
+ * Adds an order change event handler to a SwatChangeOrder that checks a radio
+ * button when the order changes
  *
  * @param String radio_button_id the XHTML id of the radio button to check.
  * @param SwatChangeOrder change_order the change order widget to add event
@@ -8,21 +8,13 @@
  */
 function AdminOrder(radio_button_id, change_order)
 {
-	var is_ie = (document.addEventListener) ? false : true;
-	var radio_button = document.getElementById(radio_button_id);
-	var node;
+	this.radio_button = document.getElementById(radio_button_id);
+	if (change_order instanceof SwatChangeOrder)
+		change_order.order_change_event.subscribe(
+			this.orderChangeHandler, this);
+}
 
-	for (var i = 0; i < change_order.list_div.childNodes.length; i++) {
-		node = change_order.list_div.childNodes[i];
-		if (is_ie)
-			node.attachEvent('onmousedown', mousedownEventHandler);
-		else
-			node.addEventListener('mousedown', mousedownEventHandler, false);
-	}
-
-	function mousedownEventHandler(event)
-	{
-		radio_button.checked = true;
-		return false;
-	}
+AdminOrder.prototype.orderChangeHandler = function(type, args, order)
+{
+	order.radio_button.checked = true;
 }
