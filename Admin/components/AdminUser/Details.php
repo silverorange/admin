1c8a3904a04ae014e7c13293b8b5935aa38997c3
Charles Waddell <charles@silverorange.com>
2007-06-27 13:00:18 +0000

    Part of ticket #974, added a data object to the user history details page.
    svn commit r18192

--- Admin/components/AdminUser/Details.php
+++ Admin/components/AdminUser/Details.php
@@ -5,21 +5,33 @@ require_once 'Admin/pages/AdminIndex.php';
 require_once 'Admin/AdminTableStore.php';
 require_once 'Admin/exceptions/AdminNotFoundException.php';
 require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/dataobjects/AdminUser.php';
+require_once 'Swat/SwatTableStore.php';
 
 /**
  * Details page for AdminUsers component
  *
- * @package Admin
- * @copyright 2005-2006 silverorange
+ * @package   Admin
+ * @copyright 2005-2007 silverorange
  */
 class AdminAdminUserDetails extends AdminIndex
 {
+	// {{{ private properties
+
+	private $id;
+
+	// }}}
+
 	// init phase
 	// {{{ protected function initInternal()
 
 	protected function initInternal()
 	{
 		$this->ui->loadFromXML(dirname(__FILE__).'/details.xml');
+
+		$this->id = intval(SiteApplication::initVar('id'));
+
+		$this->initUser();
 
 		// set a default order on the table view
 		$index_view = $this->ui->getWidget('index_view');
@@ -30,7 +42,15 @@ class AdminAdminUserDetails extends AdminIndex
 	}
 
 	// }}}
+	// {{{ protected function initUser()
+	protected function initUser()
+	{
+		$this->user = new AdminUser();
+		$this->user->setDatabase($this->app->db);
+		$this->user->load($this->id);
+	}
 
+	// }}}
 	// build phase
 	// {{{ protected function buildInternal()
 
@@ -38,14 +58,13 @@ class AdminAdminUserDetails extends AdminIndex
 	{
 		parent::buildInternal();
 
-		$id = $this->app->initVar('id');
-
 		$name = SwatDB::queryOneFromTable($this->app->db, 'AdminUser',
-			'text:name', 'id', $id);
+			'text:name', 'id', $this->id);
 
 		if ($name === null)
 			throw new AdminNotFoundException(
-				sprintf(Admin::_('User with id “%s” not found.'), $id));
+				sprintf(Admin::_('User with id “%s” not found.'), 
+					$this->id));
 
 		$frame = $this->ui->getWidget('index_frame');
 		$frame->subtitle = sprintf($name);
@@ -68,18 +87,12 @@ class AdminAdminUserDetails extends AdminIndex
 
 	protected function getTableStore($view)
 	{
-		$id = $this->app->initVar('id');
-	
-		$sql = 'select login_date, login_agent, remote_ip
-				from AdminUserHistory
-				where usernum = %s
-				order by %s';
+		$store = new SwatTableStore();
 
-        	$sql = sprintf($sql,
-			$this->app->db->quote($id, 'integer'),
-			$this->getOrderByClause($view, 'login_date desc'));
+		$user_history = $this->user->history;
 
-		$store = SwatDB::query($this->app->db, $sql, 'AdminTableStore');
+		foreach ($user_history as $history)
+			$store->addRow($history);
 
 		return $store;
 	}
