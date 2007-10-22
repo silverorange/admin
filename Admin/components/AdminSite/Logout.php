1d18d12918f29c54ef5e3d30ca48c6795dcb0b9a
Isaac Grant <isaac@silverorange.com>
2007-10-22 17:32:04 +0000

    fix the adminnotfound exception thats been firing on veseys fairly regularly.  Was caused by calling the page directly, and so there was no post vars for form->isProcessed() - thanks to mike for figuring out what was going on
    svn commit r21757

--- Admin/components/AdminSite/Logout.php
+++ Admin/components/AdminSite/Logout.php
@@ -19,30 +19,26 @@ class AdminAdminSiteLogout extends AdminPage
 		$form = $this->layout->logout_form;
 		$form->process();
 
-		if ($form->isProcessed()) {
-			if (!$form->isAuthenticated()) {
-				// add error message
-				$message = new SwatMessage(Admin::_('Unable to log out.'),
-					SwatMessage::WARNING);
-
-				$message->secondary_content =
-					Admin::_('In order to ensure your security, we were '.
-					'unable to process your logout request. Please try again.');
-
-				$this->app->messages->add($message);
-
-				// go back where we came from
-				$url = (isset($_SERVER['HTTP_REFERER'])) ?
-					$_SERVER['HTTP_REFERER'] : 'Front';
-
-				$this->app->relocate($url);
-			} else {
-				// log out
-				$this->app->session->logout();
-				$this->app->relocate($this->app->getBaseHref());
-			}
+		if ($form->isProcessed() && $form->isAuthenticated()) {
+			// log out
+			$this->app->session->logout();
+			$this->app->relocate($this->app->getBaseHref());
 		} else {
-			throw new AdminNotFoundException();
+			// add error message
+			$message = new SwatMessage(Admin::_('Unable to log out.'),
+				SwatMessage::WARNING);
+
+			$message->secondary_content =
+				Admin::_('In order to ensure your security, we were '.
+				'unable to process your logout request. Please try again.');
+
+			$this->app->messages->add($message);
+
+			// go back where we came from
+			$url = (isset($_SERVER['HTTP_REFERER'])) ?
+				$_SERVER['HTTP_REFERER'] : 'Front';
+
+			$this->app->relocate($url);
 		}
 	}
 
