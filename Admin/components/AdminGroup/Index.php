36cdab77489728ba8c1cd5509390d68c725c717f
Charles Waddell <charles@silverorange.com>
2008-01-07 17:59:59 +0000

    Lisences, whitespace, and docs.
    svn commit r25443

--- Admin/components/AdminGroup/Index.php
+++ Admin/components/AdminGroup/Index.php
@@ -10,6 +10,7 @@ require_once 'Swat/SwatTableStore.php';
  *
  * @package   Admin
  * @copyright 2005-2006 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class AdminAdminGroupIndex extends AdminIndex
 {
