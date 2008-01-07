49b5ce06ff1a4bd9585a7bd85f5a2ce39e22f20c
Charles Waddell <charles@silverorange.com>
2008-01-07 18:18:04 +0000

    Licence
    svn commit r25453

--- Admin/components/AdminUser/include/HistoryCellRenderer.php
+++ Admin/components/AdminUser/include/HistoryCellRenderer.php
@@ -8,6 +8,7 @@ require_once 'Swat/SwatHtmlTag.php';
  *
  * @package   Admin
  * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class AdminUserHistoryCellRenderer extends SwatCellRenderer
 {
