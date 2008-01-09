aa0822dccee20a0f0de0e7f6b765d8ec4fba4861
Charles Waddell <charles@silverorange.com>
2008-01-09 19:31:53 +0000

    The ability to load a sub-component from a its shortname.
    svn commit r25636

--- Admin/dataobjects/AdminSubComponent.php
+++ Admin/dataobjects/AdminSubComponent.php
@@ -20,7 +20,7 @@ class AdminSubComponent extends SwatDBDataObject
 	/**
 	 * The component that contains this sub-component 
 	 *
-	 * not sure what @var
+	 * @var string
 	 */
 	public $component;
 
@@ -83,6 +83,41 @@ class AdminSubComponent extends SwatDBDataObject
 	 * @var boolean
 	 */
 	public $show;
+
+	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads an admin sub-component by its shortname
+	 *
+	 * @param string $shortname the shortname of the sub-component to load.
+	 *
+	 * @return boolean true if loading this sub-component was successful and
+	 *                  false if a sub-component with the given shortname does
+	 *                  not exist.
+	 */
+	public function loadFromShortname($shortname)
+	{
+		$this->checkDB();
+
+		$row = null;
+
+		if ($this->table !== null) {
+			$sql = sprintf('select * from %s where shortname = %s',
+			$this->table,
+			$this->db->quote($shortname, 'text'));
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row === null)
+			return false;
+
+		$this->initFromRow($row);
+		$this->generatePropertyHashes();
+		return true;
+	}
 
 	// }}}
 	// {{{ protected function init()
