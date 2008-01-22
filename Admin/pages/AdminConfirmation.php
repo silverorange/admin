dee8df7fd957edbf1e7432805bbe65d132b300dc
Nick Burka <nick@silverorange.com>
2008-01-22 19:22:12 +0000

    ui_xml for Confirmation too
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@26325 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/pages/AdminConfirmation.php
+++ Admin/pages/AdminConfirmation.php
@@ -15,13 +15,19 @@ require_once 'Admin/AdminUI.php';
  */
 abstract class AdminConfirmation extends AdminPage
 {
+	// {{{ protected properties
+
+	protected $ui_xml = 'Admin/pages/confirmation.xml';
+
+	// }}}
+
 	// init phase
 	// {{{ protected function initInternal()
 
 	protected function initInternal()
 	{
 		parent::initInternal();
-		$this->ui->loadFromXML(dirname(__FILE__).'/confirmation.xml');
+		$this->ui->loadFromXML($this->ui_xml);
 		$this->navbar->createEntry(Admin::_('Confirmation'));
 	}
 
