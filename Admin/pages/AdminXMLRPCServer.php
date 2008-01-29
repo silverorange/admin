56c8b0761811f082d65df0ecf8b38d7d1958ead8
Michael Gauthier <mike@silverorange.com>
2008-01-29 18:38:32 +0000

    Don't throw an exception when navigating to an XML-RPC server in the admin without posting a request.
    git-svn-id: file:///Users/gauthierm/svnrepo/admin/trunk@26783 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/pages/AdminXMLRPCServer.php
+++ Admin/pages/AdminXMLRPCServer.php
@@ -1,13 +1,9 @@
 <?php
 
 require_once 'Site/pages/SiteXMLRPCServer.php';
-require_once 'Admin/exceptions/AdminNotFoundException.php';
 
 /**
  * An XML-RPC server for admin applications
- *
- * This server will throw an AdminNotFoundException if no HTTP POST data is
- * provided with the page request.
  *
  * @package   Admin
  * @copyright 2006 silverorange
@@ -15,18 +11,6 @@ require_once 'Admin/exceptions/AdminNotFoundException.php';
  */
 class AdminXMLRPCServer extends SiteXMLRPCServer
 {
-	// {{{ public function init()
-
-	/**
-	 * @xmlrpc.hidden
-	 */
-	public function init()
-	{
-		if (!isset($GLOBALS['HTTP_RAW_POST_DATA']))
-			throw new AdminNotFoundException(Admin::_('Page not found.'));
-	}
-
-	// }}}
 }
 
 ?>
