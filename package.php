cb41cb8c37c6e476d6f9e0481189db7f1ccee332
Nathan Fredrickson <nathan@silverorange.com>
2008-04-10 01:33:00 +0000

    prepare for release of 1.3.8
    svn commit r30207

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.3.7';
+$version = '1.3.8';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -53,8 +53,8 @@ $package->addReplacement('Admin/Admin.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.4');
-$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.20');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.12');
+$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.25');
 $package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.5');
 $package->generateContents();
 
