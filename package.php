06b5accdaa518586b8907ec86379e5a15d48ff83
Keith Burgoyne <keith@silverorange.com>
2008-04-14 19:49:18 +0000

    prepare for release of 1.3.9
    svn commit r30497

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.3.8';
+$version = '1.3.9';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -53,8 +53,8 @@ $package->addReplacement('Admin/Admin.php', 'pear-config', '@DATA-DIR@', 'data_d
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.12');
-$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.25');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.3.15');
+$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.2.27');
 $package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.5');
 $package->generateContents();
 
