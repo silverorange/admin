1df27843b467d35909acc71271384ca208ec4834
Charles Waddell <charles@silverorange.com>
2008-06-21 18:11:54 +0000

    Fixup the mysql getAdminMenu() function.
    svn commit r34069

--- sql/functions/getAdminMenu.mysql.sql
+++ sql/functions/getAdminMenu.mysql.sql
@@ -1,32 +1,30 @@
 CREATE PROCEDURE getAdminMenu(param_userid integer)
-	BEGIN
-		SELECT AdminComponent.shortname, AdminComponent.title,
-			AdminComponent.section, AdminSection.title AS sectiontitle,
-			AdminComponent.id,
-			AdminSubComponent.title as subcomponent_title,
-			AdminSubCOmponent.shortname as subcomponent_shortname
-		FROM AdminComponent
+	SELECT AdminComponent.shortname, AdminComponent.title,
+		AdminComponent.section, AdminSection.title AS sectiontitle,
+		AdminComponent.id,
+		AdminSubComponent.title as subcomponent_title,
+		AdminSubComponent.shortname as subcomponent_shortname
+	FROM AdminComponent
 
-			LEFT OUTER JOIN AdminSubComponent on
-				AdminSubComponent.component = AdminComponent.id and
-				AdminSUbComponent.visible = true
+		LEFT OUTER JOIN AdminSubComponent on
+			AdminSubComponent.component = AdminComponent.id and
+			AdminSubComponent.visible = true
 
-			INNER JOIN AdminSection ON
-				AdminComponent.section = AdminSection.id
+		INNER JOIN AdminSection ON
+			AdminComponent.section = AdminSection.id
 
-		WHERE AdminSection.visible = true AND
-			AdminComponent.enabled = true AND
-			AdminComponent.visible = true AND
-			AdminComponent.id IN (
-			SELECT component
-			FROM AdminComponentAdminGroupBinding
-				INNER JOIN AdminUserAdminGroupBinding ON
-					AdminComponentAdminGroupBinding.groupnum =
-						AdminUserAdminGroupBinding.groupnum
-			WHERE AdminUserAdminGroupBinding.usernum = param_userid)
+	WHERE AdminSection.visible = true AND
+		AdminComponent.enabled = true AND
+		AdminComponent.visible = true AND
+		AdminComponent.id IN (
+		SELECT component
+		FROM AdminComponentAdminGroupBinding
+			INNER JOIN AdminUserAdminGroupBinding ON
+				AdminComponentAdminGroupBinding.groupnum =
+					AdminUserAdminGroupBinding.groupnum
+		WHERE AdminUserAdminGroupBinding.usernum = param_userid)
 
-		ORDER BY AdminSection.displayorder, AdminSection.title,
-			AdminComponent.section, AdminComponent.displayorder,
-			AdminComponent.title, AdminSubComponent.displayorder,
-			AdminSubComponent.title;
-	END;
+	ORDER BY AdminSection.displayorder, AdminSection.title,
+		AdminComponent.section, AdminComponent.displayorder,
+		AdminComponent.title, AdminSubComponent.displayorder,
+		AdminSubComponent.title;
