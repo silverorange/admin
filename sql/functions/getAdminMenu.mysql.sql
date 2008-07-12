0c90a8226a72625eb2109de369733ab538f45fb8
Charles Waddell <charles@silverorange.com>
2008-07-12 02:35:00 +0000

    AdminMenuStore looks for section_title not sectiontitle.
    svn commit r34778

--- sql/functions/getAdminMenu.mysql.sql
+++ sql/functions/getAdminMenu.mysql.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE getAdminMenu(param_userid integer)
 	SELECT AdminComponent.shortname, AdminComponent.title,
-		AdminComponent.section, AdminSection.title AS sectiontitle,
+		AdminComponent.section, AdminSection.title AS section_title,
 		AdminComponent.id AS component_id,
 		AdminSubComponent.title AS subcomponent_title,
 		AdminSubComponent.shortname AS subcomponent_shortname
