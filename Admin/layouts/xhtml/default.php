b62b2c0098cb08ebc524a4a15f16c78cd3d5adca
Michael Gauthier <mike@silverorange.com>
2012-03-15 19:08:23 +0000

    HTML5 default layout for admins.
    svn commit r70101

--- Admin/layouts/xhtml/default.php
+++ Admin/layouts/xhtml/default.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html> <!--<![endif]-->
 <head>
+	<meta charset="utf-8">
 	<title><?=$this->title?></title>
-	<base href="<?=$this->basehref?>"></base>
+	<!--if IE]><base href="<?= $this->basehref ?>"></base><![endif]-->
+	<!--if !(IE)]><!--><base href="<?= $this->basehref ?>" /><!--<![endif]-->
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="icon" href="../favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
 	<?=$this->html_head_entries?>
 </head>
 <body class="<?= implode(' ', $this->body_classes->getArrayCopy()) ?>">
