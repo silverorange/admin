88498589d56fc1c94f2f715ae9b68f7d3b95ed0d
Nick Burka <nick@silverorange.com>
2012-11-15 21:13:20 +0000

    This method is useful on the front-end as well. Moving it to SiteApplication
    git-svn-id: file:///Users/gauthierm/svnrepo/public/admin/trunk@76400 94490950-a86c-4493-9ccb-530f72933e47

--- Admin/AdminApplication.php
+++ Admin/AdminApplication.php
@@ -299,39 +299,6 @@ class AdminApplication extends SiteWebApplication
 	}
 
 	// }}}
-	// {{{ public function addToSearchQueue()
-
-	public function addToSearchQueue($document_type, $document_id)
-	{
-		$manager = $this->db->manager;
-		if (!in_array('nategosearchqueue', $manager->listTables())) {
-			return;
-		}
-
-		require_once 'NateGoSearch/NateGoSearch.php';
-		$type = NateGoSearch::getDocumentType($this->db,
-			$document_type);
-
-		if ($type === null) {
-			return;
-		}
-
-		$sql = sprintf('delete from NateGoSearchQueue
-			where document_id = %s and document_type = %s',
-			$this->db->quote($document_id, 'integer'),
-			$this->db->quote($type, 'integer'));
-
-		SwatDB::exec($this->db, $sql);
-
-		$sql = sprintf('insert into NateGoSearchQueue
-			(document_id, document_type) values (%s, %s)',
-			$this->db->quote($document_id, 'integer'),
-			$this->db->quote($type, 'integer'));
-
-		SwatDB::exec($this->db, $sql);
-	}
-
-	// }}}
 	// {{{ public function hasComponent()
 
 	public function hasComponent($shortname)
