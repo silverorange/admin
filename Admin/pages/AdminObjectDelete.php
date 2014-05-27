6f112d850de237d0bed8b0b204ff0da65ed37642
Isaac Grant <isaac@silverorange.com>
2014-05-26 21:12:04 -0300

    Add an abstract base class to make deleting objects easier.
    Similar to AdminObjectEdit, but not quite as polished for now.

---
+++ Admin/pages/AdminObjectDelete.php
@@ -0,0 +1,203 @@
+<?php
+
+require_once 'Admin/pages/AdminDBDelete.php';
+
+/**
+ * Admin delete page for SwatDBDataObjects
+ *
+ * @package   Admin
+ * @copyright 2014 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+abstract class AdminObjectDelete extends AdminDBDelete
+{
+	// {{{ protected properties
+
+	/**
+	 * The dataobject instance we are editing on this page.
+	 *
+	 * @var SwatDBRecordsetWrapper
+	 *
+	 * @see AdminObjectDelete::getObjects()
+	 */
+	protected $objects;
+
+	// }}}
+	// {{{ abstract protected function getRecordsetWrapperClass()
+
+	abstract protected function getRecordsetWrapperClass();
+
+	// }}}
+	// {{{ protected function getUiXml()
+
+	protected function getUiXml()
+	{
+		return 'Admin/pages/confirmation.xml';
+	}
+
+	// }}}
+	// {{{ protected function getResolvedRecordsetWrapperClass()
+
+	protected function getResolvedRecordsetWrapperClass()
+	{
+		return SwatDBClassMap::get($this->getRecordsetWrapperClass());
+	}
+
+	// }}}
+	// {{{ protected function getObjects()
+
+	protected function getObjects()
+	{
+		return $this->objects;
+	}
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		// AdminConfirmation still requires $ui_xml be set to load custom xml.
+		$this->ui_xml = $this->getUiXml();
+
+		parent::initInternal();
+
+		$this->initObjects();
+	}
+
+	// }}}
+	// {{{ protected function initObjects()
+
+	protected function initObjects()
+	{
+		$wrapper_class = $this->getResolvedRecordsetWrapperClass();
+
+		$this->objects = SwatDB::query(
+			$this->app->db,
+			$this->getObjectsSql(),
+			$wrapper_class
+		);
+
+		if ($this->app->hasModule('SiteMemcacheModule')) {
+			$this->objects->setFlushableCache(
+				$this->app->getModule('SiteMemcacheModule')
+			);
+		}
+
+		if (count($this->objects) === 0) {
+			throw new AdminNotFoundException(
+				sprintf(
+					'No rows loaded for ‘%s’ wrapper',
+					$wrapper_class
+				)
+			);
+		}
+	}
+
+	// }}}
+	// {{{ abstract protected function getObjectsSql()
+
+	abstract protected function getObjectsSql();
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processDBData()
+
+	protected function processDBData()
+	{
+		parent::processDBData();
+
+		$objects = $this->getObjects();
+		$delete_count = count($objects);
+
+		$this->deleteObjects($objects);
+
+		$message = $this->getDeletedMessage($delete_count);
+		if ($message instanceof SiteMessage) {
+			$this->app->messages->add($message);
+		}
+	}
+
+	// }}}
+	// {{{ protected function deleteObjects()
+
+	protected function deleteObjects(SwatDBRecordsetWrapper $objects)
+	{
+		foreach ($objects as $object) {
+			$this->deleteObject($object);
+		}
+	}
+
+	// }}}
+	// {{{ protected function deleteObject()
+
+	protected function deleteObject(SwatDBDataObject $object)
+	{
+		$object->delete();
+	}
+
+	// }}}
+	// {{{ protected function getDeletedMessage()
+
+	protected function getDeletedMessage()
+	{
+		$message = null;
+
+		$message_type      = $this->getSavedMessageType();
+		$primary_content   = $this->getSavedMessagePrimaryContent();
+		$secondary_content = $this->getSavedMessageSecondaryContent();
+		$content_type      = $this->getSavedMessageContentType();
+
+		if ($primary_content != '') {
+			$message = new SwatMessage($primary_content, $message_type);
+
+			if ($secondary_content != '') {
+				$message->secondary_content = $secondary_content;
+			}
+
+			if ($content_type != '') {
+				$message->content_type = $content_type;
+			}
+		}
+
+		return $message;
+	}
+
+	// }}}
+	// {{{ protected function getSavedMessagePrimaryContent()
+
+	protected function getSavedMessagePrimaryContent($delete_count)
+	{
+		return null;
+	}
+
+	// }}}
+	// {{{ protected function getSavedMessageSecondaryContent()
+
+	protected function getSavedMessageSecondaryContent()
+	{
+		return null;
+	}
+
+	// }}}
+	// {{{ protected function getSavedMessageType()
+
+	protected function getSavedMessageType()
+	{
+		return null;
+	}
+
+	// }}}
+	// {{{ protected function getSavedMessageContentType()
+
+	protected function getSavedMessageContentType()
+	{
+		return null;
+	}
+
+	// }}}
+}
+
+?>
