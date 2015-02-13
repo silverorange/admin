f731fec0cfe5a73e8f292ccab2967b5e51dda3c8
Michael Gauthier <mike@silverorange.com>
2015-02-13 18:54:49 -0400

    Ever get an exception in the admin? Well now it will look good.

--- Admin/components/AdminSite/Exception.php
+++ Admin/components/AdminSite/Exception.php
@@ -1,9 +1,10 @@
 <?php
 
+require_once 'Swat/SwatContentBlock.php';
+require_once 'Swat/SwatContainer.php';
+require_once 'Swat/SwatFrame.php';
 require_once 'Admin/layouts/AdminDefaultLayout.php';
 require_once 'Site/pages/SiteXhtmlExceptionPage.php';
-require_once 'Swat/SwatMessage.php';
-require_once 'Swat/SwatMessageDisplay.php';
 
 /**
  * Exception page in an admin application
@@ -13,12 +14,33 @@ require_once 'Swat/SwatMessageDisplay.php';
  */
 class AdminAdminSiteException extends SiteXhtmlExceptionPage
 {
+	// {{{ protected properties
+
+	/**
+	 * @var SwatContainer
+	 */
+	protected $container;
+
+	// }}}
 	// {{{ protected function createLayout()
 
 	protected function createLayout()
 	{
 		return new AdminDefaultLayout($this->app,
 			'Admin/layouts/xhtml/default.php');
+	}
+
+	// }}}
+
+	// init phase
+	// {{{ public function init()
+
+	public function init()
+	{
+		parent::init();
+
+		$this->container = new SwatFrame();
+		$this->container->classes[] = 'admin-exception-container';
 	}
 
 	// }}}
@@ -41,12 +63,35 @@ class AdminAdminSiteException extends SiteXhtmlExceptionPage
 
 	protected function display()
 	{
+		ob_start();
+
 		printf('<p>%s</p>', $this->getSummary());
 
 		echo '<p>This error has been reported.</p>';
 
-		if ($this->exception !== null)
+		if ($this->exception !== null) {
 			$this->exception->process(false);
+		}
+
+		$content_block = new SwatContentBlock();
+		$content_block->content = ob_get_clean();
+		$content_block->content_type = 'text/xml';
+
+		$this->container->add($content_block);
+		$this->container->display();
+	}
+
+	// }}}
+
+	// finalize phase
+	// {{{ public function finalize()
+
+	public function finalize()
+	{
+		parent::finalize();
+		$this->layout->addHtmlHeadEntrySet(
+			$this->container->getHtmlHeadEntrySet()
+		);
 	}
 
 	// }}}
