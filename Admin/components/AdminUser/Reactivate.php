9d8ed820bf28c291265e8ecb45c9512f0c8040bf
Michael Gauthier <mike@silverorange.com>
2016-03-10 00:51:38 -0400

    Add tool to reactivate user accounts.

---
+++ Admin/components/AdminUser/Reactivate.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once 'Admin/pages/AdminDBDelete.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Admin/AdminListDependency.php';
+
+/**
+ * Reactivate confirmation page for AdminUsers component
+ *
+ * @package   Admin
+ * @copyright 2016 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class AdminAdminUserReactivate extends AdminDBConfirmation
+{
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->navbar->popEntry();
+		$this->navbar->createEntry(Admin::_('Reactivate'));
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processDBData
+
+	protected function processDBData()
+	{
+		parent::processDBData();
+
+		$locale = SwatI18NLocale::get();
+
+		$now = new SwatDate();
+		$now->toUTC();
+
+		$sql = sprintf(
+			'update AdminUser set createdate = %s
+			where id in (%s)',
+			$this->app->db->quote($now->getDate(), 'date'),
+			$this->getItemList('integer')
+		);
+
+		$num = SwatDB::exec($this->app->db, $sql);
+
+		$this->app->messages->add(
+			new SwatMessage(
+				sprintf(
+					Admin::ngettext(
+						'One admin user has been reactivated.',
+						'%s admin users have been reactivated.',
+						$num
+					),
+					$locale->formatNumber($num)
+				)
+			)
+		);
+
+	}
+
+	// }}}
+
+	// build phase
+	// {{{ protected function buildInternal()
+
+	public function buildInternal()
+	{
+		parent::buildInternal();
+
+		$users = SwatDB::query(
+			$this->app->db,
+			sprintf(
+				'select name from AdminUser
+				where id in (%s)
+				order by name asc',
+				$this->getItemList('integer')
+			),
+			'AdminUserWrapper'
+		);
+
+		ob_start();
+
+		$h3_tag = new SwatHtmlTag('h3');
+		$h3_tag->setContent(
+			Admin::ngettext(
+				'Reactivate the following admin user?',
+				'Reactivate the following admin users?',
+				count($users)
+			)
+		);
+		$h3_tag->display();
+
+		if (count($users) > 0) {
+			echo '<ul>';
+			foreach ($users as $user) {
+				$li_tag = new SwatHtmlTag('li');
+				$li_tag->setContent($user->name);
+				$li_tag->display();
+			}
+			echo '</ul>';
+		}
+
+		$message = $this->ui->getWidget('confirmation_message');
+		$message->content = ob_get_clean();
+		$message->content_type = 'text/xml';
+
+		if (count($users) === 0) {
+			$this->switchToCancelButton();
+		}
+	}
+
+	// }}}
+}
+
+?>
