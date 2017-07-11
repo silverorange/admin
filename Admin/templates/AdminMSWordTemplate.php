16d6d0a50952c99140c0efe523b07f3e86595b20
Michael Gauthier <mike@silverorange.com>
2017-07-11 15:09:52 -0300

    Extend base template class

--- Admin/templates/AdminMSWordTemplate.php
+++ Admin/templates/AdminMSWordTemplate.php
@@ -5,7 +5,7 @@
  * @copyright 2017 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class AdminMSWordTemplate implements SiteTemplateInterface
+class AdminMSWordTemplate extends SiteAbstractTemplate
 {
 	// {{{ public function display()
 
