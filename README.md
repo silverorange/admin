6b05d874491c5fc9122b578be830663ee6609aff
Colin Viebrock <colin@viebrock.ca>
2024-09-18 10:16:58 -0500

    Run prettier on codebase

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Admin
-=====
+# Admin
+
 Admin is a framework for back-end administration systems. Admin is built using
 [Swat](https://github.com/silverorange/swat) and
 [Site](https://github.com/silverorange/site).
 
-Installation
------------
+## Installation
+
 Make sure the silverorange composer repository is added to the `composer.json`
 for the project and then run:
 
@@ -13,31 +13,31 @@ for the project and then run:
 composer require silverorange/admin
 ```
 
-Enabling 2FA (Two Factor Authentication)
------------
+## Enabling 2FA (Two Factor Authentication)
+
 1. Install the Admin package ≥ `6.1.0`
 2. Add two composer packages:
 
-  ```sh
-  composer require robthree/twofactorauth
-  composer require bacon/bacon-qr-code
-  ```
+```sh
+composer require robthree/twofactorauth
+composer require bacon/bacon-qr-code
+```
 
 3. Run `composer install`
 
 4. Add the new database fields:
 
-  ```sql
-  alter table adminuser add two_fa_secret varchar(255);
-  alter table adminuser add two_fa_enabled boolean not null default false;
-  alter table adminuser add two_fa_timeslice integer not null default 0;
-  ```
+```sql
+alter table adminuser add two_fa_secret varchar(255);
+alter table adminuser add two_fa_enabled boolean not null default false;
+alter table adminuser add two_fa_timeslice integer not null default 0;
+```
 
 5. Edit your `.ini` files (both stage and production) and add:
 
-  ```
-  [admin]
-  two_fa_enabled = On
-  ```
+```
+[admin]
+two_fa_enabled = On
+```
 
 6. Let your users know! They will now see 2FA setup in the “Login Settings” in the top-right corner.
--- www/javascript/admin-login.js
+++ www/javascript/admin-login.js
@@ -1,29 +1,44 @@
 /**
  * Override the default SwatForm focus behaviour
  */
-SwatForm.prototype.setDefaultFocus = function()
-{
-};
+SwatForm.prototype.setDefaultFocus = function () {};
 
-function AdminLogin(email_id, password_id, submit_name, user_email, login_error)
-{
-	setTimeout(
-	"email = document.getElementById('" + email_id + "');"+
-	"password = document.getElementById('" + password_id + "');"+
-	"submit = document.getElementById('" + submit_name + "');"+
-	"if (" + login_error + ") {" +
-	"	if (email.value.length && email.value == '" + user_email + "') {"+
-	"		password.focus();"+
-	"	} else {"+
-	"		email.focus();"+
-	"	}"+
-	"} else {" +
-	"	if (email.value.length > 0 && password.value.length > 0) {" +
-	"		submit.focus();" +
-	"	} else if (email.value.length > 0 && password.value.length == 0) {" +
-	"		password.focus();" +
-	"	} else {" +
-	"		email.focus();" +
-	"	}" +
-	"}", 100);
+function AdminLogin(
+  email_id,
+  password_id,
+  submit_name,
+  user_email,
+  login_error
+) {
+  setTimeout(
+    "email = document.getElementById('" +
+      email_id +
+      "');" +
+      "password = document.getElementById('" +
+      password_id +
+      "');" +
+      "submit = document.getElementById('" +
+      submit_name +
+      "');" +
+      'if (' +
+      login_error +
+      ') {' +
+      "	if (email.value.length && email.value == '" +
+      user_email +
+      "') {" +
+      '		password.focus();' +
+      '	} else {' +
+      '		email.focus();' +
+      '	}' +
+      '} else {' +
+      '	if (email.value.length > 0 && password.value.length > 0) {' +
+      '		submit.focus();' +
+      '	} else if (email.value.length > 0 && password.value.length == 0) {' +
+      '		password.focus();' +
+      '	} else {' +
+      '		email.focus();' +
+      '	}' +
+      '}',
+    100
+  );
 }
